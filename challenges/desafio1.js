5266c2de35a2b4b517ffbd68816d22686b022f0f
guilhermemd <guilherme.md@gmail.com>
2021-08-16 19:46:18 -0300

    feat: :sparkles: req 01 implementation

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,1 +1,4 @@
 // 1 - Inclua o campo criadoPor em todos os documentos, colocando "Ronald McDonald" no valor desse campo.
+// Crie uma query que retorne o nome e criadoPor de todos os produtos.
+db.produtos.updateMany({}, { $set: { criadoPor: "Ronald McDonald" } });
+db.produtos.find({}, { criadoPor: 1, nome: 1, _id: 0 });

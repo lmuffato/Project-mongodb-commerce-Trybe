6def753ab29815b6736cf9f45273495f1cb1e75a
carlos25xrm <carlos25xrm@gmail.com>
2021-08-16 20:34:57 -0300

    feat: desafio 3

---
+++ challenges/desafio3.js
@@ -0,0 +1,9 @@
+db.produtos.updateMany({ }, { $set: { avaliacao: NumberInt(0) } });
+
+db.produtos.updateMany({ tags: { $in: [ "bovino" ] } }, { $inc: { avaliacao: NumberInt(5) } });
+
+db.produtos.updateMany({ tags: { $in: [ "ave" ] } }, { $inc: { avaliacao: NumberInt(3) } });
+
+db.produtos.find({}, { nome: 1, avaliacao: 1, _id:0 });
+    
+// https://docs.mongodb.com/manual/core/shell-types/#numberint

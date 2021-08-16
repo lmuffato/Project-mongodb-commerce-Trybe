f3d78c22d689993fa1d9533261b0f20f9d39cf9c
Rafael Geronimo <rafaelgeronimo@outlook.com>
2021-08-16 18:58:15 +0000

    feat: challenge 1

---
+++ challenges/desafio1.js
@@ -0,0 +1,6 @@
+// 1 - Inclua o campo criadoPor em todos os documentos, colocando "Ronald McDonald"
+// no valor desse campo.
+
+db.produtos.updateMany({ }, { $set: { criadoPor: "Ronald McDonald" } });
+
+db.produtos.find({ }, { nome: 1, criadoPor: 1, _id: 0 }).pretty();

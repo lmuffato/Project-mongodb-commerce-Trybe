b86b5bf840691a8872935c882975ab1e41930e9e
Robertson <robertson.nunes@gmail.com>
2021-08-17 15:09:55 -0300

    Requisito 13

---
+++ challenges/desafio13.js
@@ -0,0 +1,10 @@
+// 13 - Adicione o elemento muito sódio ao final do array tags nos produtos em que o percentual de sódio seja maior ou igual a 40.
+
+db.produtos.updateMany(
+  { valoresNutricionais:
+    { $elemMatch:
+      { tipo: "sódio",
+      percentual: { $gte: 40 } } } }, { $push: { tags: "muito sódio" } },
+);
+
+db.produtos.find({ }, { nome: 1, tags: 1, _id: 0 }); 

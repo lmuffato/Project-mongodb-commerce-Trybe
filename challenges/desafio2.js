fdbe1b41c89924a1efce805af0f6539f7512aa9b
gabrieldeori <deori.pessoal@gmail.com>
2021-08-17 12:17:59 -0300

    📌 Add desafio2

---
+++ challenges/desafio2.js
@@ -0,0 +1,24 @@
+db.produtos.updateMany(
+  {
+    valorUnitario: {
+      $exists: false
+    }
+  },
+  {
+    $set: {
+      valorUnitario: NumberDecimal("0.00"),
+    },
+  },
+  {
+    upsert: true,
+  },
+);
+
+db.produtos.find(
+  {},
+  {
+    _id: 0,
+    nome: 1,
+    valorUnitario: 1,
+  },
+);

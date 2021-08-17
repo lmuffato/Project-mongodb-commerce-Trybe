bf12718136013004bd1287ef032e1599f4917f23
Felipe Muller <felipeavmuller@gmail.com>
2021-08-17 17:01:56 -0300

    Desafio 4

---
+++ challenges/desafio4.js
@@ -0,0 +1,13 @@
+db.produtos.updateOne(
+  { nome: "Big Mac" },
+  {
+    $set: {
+      ultimaModificacao: Date(),
+    },
+  },
+);
+
+db.produtos.find(
+  { ultimaModificacao: { $exists: true } },
+  { nome: 1, _id: 0 },
+);

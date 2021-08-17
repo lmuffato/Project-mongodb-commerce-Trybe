c53ad0a7ce2b70fce09fe9c3454239644d2f8d34
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-08-17 16:48:16 -0300

    correcoes eslint

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -1,9 +1,9 @@
 db.produtos.updateMany(
   { valorUnitario: { $exists: false } },
-  { $set: { valorUnitario: NumberDecimal("0.00") } }
+  { $set: { valorUnitario: NumberDecimal("0.00") } },
 );
 
 db.produtos.find(
   {},
-  { nome: 1, valorUnitario: 1, _id: 0 }
+  { nome: 1, valorUnitario: 1, _id: 0 },
 );

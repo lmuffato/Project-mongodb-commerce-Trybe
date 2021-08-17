3f603ca76cb3c902b1277513db69aa5a4f39d2f8
carlos25xrm <carlos25xrm@gmail.com>
2021-08-17 12:11:05 -0300

    fix: desafio 3

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -1,4 +1,4 @@
-db.produtos.updateMany({ }, { $set: { avaliacao: NumberInt(0) } });
+db.produtos.updateMany({}, { $set: { avaliacao: NumberInt(0) } });
 
 db.produtos.updateMany({ tags: { $in: ["bovino"] } }, { $inc: { avaliacao: NumberInt(5) } });
 

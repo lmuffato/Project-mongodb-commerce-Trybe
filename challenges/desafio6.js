d5e71cf77110b68b05b241761c5a3770a34ae55e
Giovanni Maldonado <giovannitadeu08@gmail.com>
2021-08-17 17:01:31 -0300

    fix: desafio 6

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,5 +1,5 @@
 db.produtos.updateMany(
   { nome: { $in: ["Bic Mac", "Quarteirão com Queijo"] } },
-  { $push: { ingredientes: "bacon" } },
+  { $addToSet: { ingredientes: "bacon" } },
 );
-db.produtos.find({}, { nome: 1, ingredientes: 1, _id: 0 });
+db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });

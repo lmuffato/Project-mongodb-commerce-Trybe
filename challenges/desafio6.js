ffad52dc5f4728b87ad41ea17b2300e7d330b441
Marina Alane <marina.alane@outlook.com>
2021-08-17 16:31:49 -0300

    desafio 6 corrigido

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,8 +1,10 @@
 // Crie uma query que faça a inclusão de bacon no
 // final da lista de ingredientes dos
 // sanduíches Big Mac e Quarteirão com Queijo.
-db.produtos.updateMany({},
-  { $pull: { ingredientes: "cebola" } });
+db.produtos.updateMany(
+  { nome: { $in: ["Big Mac", "Quarteirão com Queijo"] } },
+  { $push: { ingredientes: "bacon" } },
+);
 // Crie uma query que retorne o nome e
 // ingredientes de todos os documentos.
 db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });

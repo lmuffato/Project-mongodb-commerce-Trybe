52acfa132876864d4886315b835626f4c681b8c2
guilhermemd <guilherme.md@gmail.com>
2021-08-17 18:57:45 -0300

    feat:

---
+++ challenges/desafio17.js
@@ -0,0 +1,18 @@
+// 17 - Conte quantos documentos contêm as palavras frango e hamburguer utilizando o operador $text.
+// Para isso, escreva no arquivo desafio17.js duas queries, nesta ordem:
+
+// Crie uma query que faça a criação de um índice do tipo text no campo descricao com o idioma padrão portuguese.
+
+// Crie uma query que retorne a quantidade de documentos que contêm as palavras frango e hamburguer utilizando o operador $text.
+// https://docs.mongodb.com/manual/tutorial/specify-language-for-text-index/
+
+db.produtos.createIndex({
+  ingredientes: "text",
+}, {
+  default_language: "portuguese",
+});
+db.produtos.count({
+  $text: {
+    $search: "frango hamburguer",
+  },
+});

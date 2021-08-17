a844d9a4a4222ca5f332da96dcc7529cc9f539b3
Rafael Geronimo <rafaelgeronimo@outlook.com>
2021-08-17 18:02:41 +0000

    feat: challenge 19

---
+++ challenges/desafio19.js
@@ -0,0 +1,9 @@
+// 19 - Renomeie o campo descricao para descricaoSite em todos os documentos.
+db.produtos.updateMany(
+    {},
+    { $rename: {
+        descricao: "descricaoSite",
+    } },
+);
+
+db.produtos.find({}, { nome: 1, descricao: 1, descricaoSite: 1, _id: 0 });

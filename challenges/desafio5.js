4ce459a24b8769befe8df562e30e0d28be4f4ab2
Carolina Vasconcelos <carolinavpinheiro@gmail.com>
2021-08-17 15:59:31 -0300

    Feat: desafio 5

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,6 +1,6 @@
 db.produtos.updateMany(
   { nome: { $ne: "McChicken" } },
-  { $addToSet: { ingredientes: ["ketchup"] } },
+  { $addToSet: { ingredientes: "ketchup" } },
 );
 
 db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 }).pretty();

e2ca72357ba765fa3edbf5adb15512eb0fd0699c
Rodrigo Rech <rodrigorech92@gmail.com>
2021-08-17 17:32:11 -0300

    Fix: Desafio18

--- challenges/desafio18.js
+++ challenges/desafio18.js
@@ -1,3 +1,3 @@
 db.produtos.createIndex({ descricao: "text" }, { default_language: "portuguese" });
 
-db.produtos.count({ $text: "\"feito com\"" });
+db.produtos.count({ $text: { $search: "\"feito com\"" } });

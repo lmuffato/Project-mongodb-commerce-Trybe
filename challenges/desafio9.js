f5217d3fd16a753a07d212ee1aa8a10147f6d7a0
Robertson <robertson.nunes@gmail.com>
2021-08-17 14:41:39 -0300

    Requisito 9

---
+++ challenges/desafio9.js
@@ -0,0 +1,5 @@
+// 9 - Remova o último ingrediente do sanduíche Cheddar McMelt.
+
+db.produtos.updateMany({ nome: "Cheddar McMelt" }, { $pop: { ingredientes: 1 } });
+
+db.produtos.find({ }, { nome: 1, ingredientes: 1, _id: 0 }); 

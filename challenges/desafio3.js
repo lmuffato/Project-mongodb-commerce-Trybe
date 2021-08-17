e7701bb5b6aa00cde080ccfdd90ecd596fc447ed
Marina Alane <marina.alane@outlook.com>
2021-08-17 15:54:15 -0300

    fix lint

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -1,31 +1,27 @@
 // Crie uma query que inclua o campo avaliacao do tipo NumberInt e
 // com o valor 0 em todos os documentos da coleção.
-db.produtos.updateMany( {},
+db.produtos.updateMany({},
   { $set: {
       avaliacao: NumberInt(0),
     },
-  },
-);
+  });
 // Crie uma query que incremente o valor do campo avaliacao em 5 em todos os sanduíches de carne do tipo bovino. Dica: 
 // utilize como filtro o campo tags.
-db.produtos.updateMany( { tags: { $in: ["bovino"] } },
+db.produtos.updateMany({ tags: { $in: ["bovino"] } },
   { $inc: {
       avaliacao: 5,
     },
-  },
-);
+  });
 // Crie uma query que incremente o valor do campo avaliacao em 3 em todos os sanduíches de ave.
-db.produtos.updateMany( { tags: { $in: ["ave"] } },
+db.produtos.updateMany({ tags: { $in: ["ave"] } },
 { $inc: {
       avaliacao: 3,
     },
-  },
-);
+  });
 // Crie uma query que retorne o nome e avaliacao de todos os sanduíches.
 db.produtos.find({},
   {
     _id: false,
     nome: true,
     avaliacao: true,
-  },
-);
+  });
--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -2,9 +2,9 @@
 // para todos os sanduíches menos o McChicken,
 // garantindo que não haja duplicidade nos ingredientes
 db.produtos.updateMany(
-  { nome: { $ne: "McChicken"} },
-  { $addToSet: {ingredientes: "ketchup" } },
+  { nome: { $ne: "McChicken" } },
+  { $addToSet: { ingredientes: "ketchup" } },
 );
 
 // Crie uma query que retorne o nome e ingredientes de todos os documentos.
-db.produtos.find({}, {_id: 0, nome: 1, ingredientes:1});
+db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });

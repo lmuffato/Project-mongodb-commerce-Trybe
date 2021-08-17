5195eb69f27d6d106eaee4e7e7b6858aba9377cb
Marina Alane <marina.alane@outlook.com>
2021-08-17 17:02:42 -0300

    desafio 9 ao 11

---
+++ challenges/desafio9.js
@@ -0,0 +1,7 @@
+// Crie uma query que faça a remoção do último ingrediente no sanduíche Cheddar McMelt.
+db.produtos.updateOne(
+  { nome: { $eq: "Cheddar McMelt" } },
+  { $pop: { ingredientes: 1 } },
+);
+// Crie uma query que retorne o nome e ingredientes de todos os documentos.
+db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });

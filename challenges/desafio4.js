fb381e2615b3ed0166a1a7b24b66af308a2d33bf
Gabriel <g.pereira.a.m@gmail.com>
2021-08-17 19:47:29 -0300

    feat(Req 4) - :sparkle: Desafio 4 feito

---
+++ challenges/desafio4.js
@@ -0,0 +1,17 @@
+db.produtos.updateOne(
+  { nome: "Big Mac" },
+  {
+    $currentDate: {
+      ultimaModificacao: true,
+    },
+  },
+);
+
+db.produtos.find(
+  {
+    ultimaModificacao: {
+      $exists: true,
+    },
+  },
+  { _id: 0, nome: 1 },
+).pretty();

b02e4a2e5529168ae022216762e8efe4e194d299
Rodrigo Rech <rodrigorech92@gmail.com>
2021-08-17 15:59:23 -0300

    Feat: Desafio 10

---
+++ challenges/desafio10.js
@@ -0,0 +1,9 @@
+db.produtos.updateMany({}, { $set: { vendasPorDia: [0, 0, 0, 0, 0, 0, 0] } });
+
+db.produtos.updateOne({ nome: "Big Mac" },
+{ $push: { vendasPorDia: { $each: [60], $position: 3, $slice: 7 } } });
+
+db.produtos.updateMany({ tags: { $in: ["bovino", "pao"] } },
+{ $push: { vendasPorDia: { $each: [120], $position: 6, $slice: 7 } } });
+
+db.produtos.find({}, { _id: 0, nome: 1, vendasPorDia: 1 });

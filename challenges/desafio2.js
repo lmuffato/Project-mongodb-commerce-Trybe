257d652c3cc16d43e508e7f0bce5e3056f33d12b
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-08-17 16:57:57 -0300

    Desafio 2:Inclusao do campo valorUnitario e filtragem especifica

---
+++ challenges/desafio2.js
@@ -0,0 +1,11 @@
+db.produtos.updateMany(
+    {},
+    { $set: {
+      valorUnitario: NumberDecimal("0.00"),
+    } },
+);
+
+db.produtos.find(
+    {},
+    { nome: 1, valorUnitario: 1, _id: 0 },
+);

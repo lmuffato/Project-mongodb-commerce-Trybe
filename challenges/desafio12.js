136c99f4861fb558fdc9f8e3c51ca42170ccc6e3
carlos25xrm <carlos25xrm@gmail.com>
2021-08-17 14:48:47 -0300

    fix: desafio 12

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,5 +1,5 @@
 db.produtos.updateMany({},
-    { $push: { $each: [], $sort: { percentual: -1 } } });
+    { $push: { valoresNutricionais: { $each: [], $sort: { percentual: -1 } } } });
 
 db.produtos.find({}, { nome: 1, valoresNutricionais: 1, _id: 0 });
 

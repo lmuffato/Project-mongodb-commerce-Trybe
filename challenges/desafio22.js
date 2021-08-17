60519feb2c86c343cf241a908af8cb87d6a64705
Elisa <elisafranca.carvalho@gmail.com>
2021-08-17 16:35:53 -0300

    feat: requirement 22

---
+++ challenges/desafio22.js
@@ -0,0 +1,13 @@
+// Retorne o nome e a quantidade de vendas (vendidos) dos sanduíches em que o número de vendas é múltiplo de 5.
+db.produtos.find(
+  {
+    vendidos: {
+      $mod: [5, 0],
+    },
+  },
+  {
+    _id: false,
+    nome: true,
+    vendidos: true,
+  },
+);

34a24a068db96eaa69dd48227ac97e52ac93c3ef
Robertson <robertson.nunes@gmail.com>
2021-08-17 15:20:35 -0300

    Requisito 15

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -2,4 +2,4 @@
 
 db.produtos.updateMany({}, { $push: { tags: { $each: ["combo", "tasty"], $sort: 1 } } });
 
-db.produtos.find({ }, { nome: 1, tags: 1, _id: 0 }); 
+db.produtos.find({ }, { nome: 1, tags: 1, _id: 0 });

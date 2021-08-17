61cc93a439e9a471392249ebf62d0f80327bcd0a
luanlsr <luanfswd@gmail.com>
2021-08-16 23:14:52 -0300

    feat: :sparkles: finished req 4

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -1,6 +1,10 @@
 // 2 - Inclua o campo valorUnitario em todos os documentos em que esse campo não existe e atribua a ele o valor "0.00", com o tipo NumberDecimal.
+// Crie uma query que adicione o campo valorUnitario em todos os documentos em que esse campo não existe e atribua a ele o valor "0.00", com o tipo NumberDecimal.
+// Crie uma query que retorne o nome e valorUnitario de todos os produtos.
 db.produtos.updateMany(
-  { valorUnitario: 0.00 },
+  {},
   { $set: { valorUnitario: NumberDecimal("0.00") } },
   { upsert: true },
 );
+
+db.produtos.find({ valorUnitario: 0.00 }, { _id: 0, nome: 1, valorUnitario: 1 });
--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,1 +1,24 @@
 // 4 - Atribua a data corrente ao campo ultimaModificacao no sanduíche Big Mac.
+// Crie uma query que atribua a data corrente ao campo ultimaModificacao no sanduíche Big Mac. Para a data corrente faça uso do tipo Date.
+
+// Crie uma query que retorne o nome de todos os documentos em que o campo ultimaModificacao existe.
+db.produtos.updateMany({
+  nome: "Big Mac",
+}, {
+    $currentDate: {
+      ultimaModificacao: {
+        $type: "date",
+      },
+    },
+}, {
+  upsert: true,
+});
+
+db.produtos.find({
+  ultimaModificacao: {
+    $exists: true,
+  },
+}, {
+  _id: 0,
+  nome: 1,
+});

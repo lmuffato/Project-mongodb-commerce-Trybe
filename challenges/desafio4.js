7ad7b8ff448eda63211637369c7862ef15014d91
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-08-17 17:18:21 -0300

    fix: requisito 4, nome

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,4 +1,4 @@
-db.produtos.updateOne({}, { $set: { ultimaModificacao: new Date() } });
+db.produtos.updateOne({ nome: "Big Mac" }, { $set: { ultimaModificacao: new Date() } });
 db.produtos.find(
   { ultimaModificacao: { $existes: true } }, { avaliacao: true, nome: true, _id: false },
 );

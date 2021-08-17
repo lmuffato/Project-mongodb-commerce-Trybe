ac34833930c1d0bbcb7db11aec9c36f36856e0a1
Felipe Muller <felipeavmuller@gmail.com>
2021-08-17 17:55:10 -0300

    Desafio 12

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -3,10 +3,7 @@ db.produtos.updateMany(
   {
     $push: {
       tags: {
-        $each: [
-          "combo",
-          "tasty",
-        ],
+        $each: ["combo", "tasty"],
         $sort: 1,
       },
     },

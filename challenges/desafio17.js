67385a4ae96d10e4d15514611b5dd10aeac18cdf
Jodiel Briesemeister <jodielll29@gmail.com>
2021-08-17 16:31:24 -0300

    feat: req18

--- challenges/desafio17.js
+++ challenges/desafio17.js
@@ -1,6 +1,6 @@
 db.produtos.createIndex(
   { descricao: "text" },
-  { default_language: "portuguese" }
+  { default_language: "portuguese" },
 );
 
 db.produtos.count({ $text: { $search: "frango hamburguer" } });

eee0771f49fa6041aed8775e7281b3aef31b211b
Luciano Amâncio <madruga665@hotmail.com>
2021-08-17 20:53:24 -0300

    desafio 12

---
+++ challenges/desafio12.js
@@ -0,0 +1,14 @@
+db.produtos.updateMany({}, {
+  $push: {
+    valoresNutricionais: {
+      $each: [],
+      $sort: { percentual: -1 },
+    },
+  },
+});
+
+db.produtos.find({}, {
+  _id: false,
+  nome: true,
+  valoresNutricionais: true,
+});

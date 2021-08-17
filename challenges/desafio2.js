d1450ddc2de76a3e8e9ddc8182fef2455dad3166
lucas-chave <lcchavesr150@gmail.com>
2021-08-17 17:09:01 -0300

    fix challenge 2

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -1,5 +1,5 @@
 // challenge 2;
 
-db.produtos.updateMany({}, { $set: { valorUnitario: "0.00" } });
+db.produtos.updateMany({}, { $set: { valorUnitario: 0.00 } });
 
 db.produtos.find({ }, { nome: 1, valorUnitario: 1, _id: 0 });

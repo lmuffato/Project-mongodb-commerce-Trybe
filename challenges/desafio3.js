e8ef258eed358ab5abdbf6ea0548c07be49b3169
jonathan <jonathanfernandesjf98@gmail.com>
2021-08-17 17:44:19 -0300

    desafio 3 correcao

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -1,7 +1,7 @@
 db.produtos.updateMany({}, { $set: { avaliacao: NumberInt("0") } });
 
-db.produtos.updateMany({ tags: { $elemMatch: { $eq: "bovino" } } }, { $inc: { avaliacao: 5 } });
+db.produtos.updateMany({ tags: { $all: ["bovino"] } }, { $inc: { avaliacao: 5 } });
 
-db.produtos.updateMany({ tags: { $elemMatch: { $eq: "ave" } } }, { $inc: { avaliacao: 3 } });
+db.produtos.updateMany({ tags: { $all: ["ave"] } }, { $inc: { avaliacao: 3 } });
 
 db.produtos.find({}, { nome: 1, avaliacao: 1, _id: 0 });

e761a239a334bbb29063581a4d8c770eb962ae57
luanlsr <luanfswd@gmail.com>
2021-08-16 22:30:11 -0300

    feat: :sparkles: finished req 2

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -1,6 +1,6 @@
 // 2 - Inclua o campo valorUnitario em todos os documentos em que esse campo não existe e atribua a ele o valor "0.00", com o tipo NumberDecimal.
 db.produtos.updateMany(
-  {},
+  { valorUnitario: 0.00 },
   { $set: { valorUnitario: NumberDecimal("0.00") } },
   { upsert: true },
 );

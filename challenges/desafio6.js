780041aee3dadb23337a5751bc47b12adceaeaa5
w-cypriano <profwellington17@gmail.com>
2021-08-17 20:54:42 -0300

    desafio6

---
+++ challenges/desafio6.js
@@ -0,0 +1,22 @@
+db.produtos.updateMany(
+  { 
+    nome: 
+    { 
+        $in: ["Big Mac", "Quarteirão com Queijo"],
+    },
+  }, 
+  {
+    $push: {
+      ingredientes: {
+        $each: ["bacon"],
+      },
+    },
+  },
+);
+
+db.produtos.find(
+  {}, 
+  { 
+    nome: 1, ingredientes: 1, _id: 0,
+  },
+);

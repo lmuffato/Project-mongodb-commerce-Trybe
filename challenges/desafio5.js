9e02816606930ebc66861f1bd825290fe93653c6
Nathalia Zebral <natzebral@gmail.com>
2021-08-17 09:40:03 -0300

    desafio 5

---
+++ challenges/desafio5.js
@@ -0,0 +1,6 @@
+db.produtos.updateMany({
+    nome: { $nin: ["McChicken"] },
+    ingredientes: { $nin: ["ketchup"] } },
+    { $push: { ingredientes: "ketchup" } });
+
+db.produtos.find({}, { nome: 1, ingredientes: 1, _id: 0 });

6b11f84ff1ec7d02ada499bcec3aac1a0207e6f4
Bruno Mendes <mendessbruno@gmail.com>
2021-08-17 13:48:07 -0300

    resolvido desafio 6

---
+++ challenges/desafio6.js
@@ -0,0 +1,6 @@
+db.produtos.updateMany(
+  { $or: [{ nome: { $eq: "Big Mac" } }, { nome: { $eq: "Quarteirão com Queijo" } }] },
+  { $addToSet: { ingredientes: "bacon" } },
+);
+
+db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });

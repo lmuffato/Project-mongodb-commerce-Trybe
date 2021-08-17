c237b9255e8417baca54163138057ed7fe5061e4
Micaela Alves <74266352+malves0@users.noreply.github.com>
2021-08-17 14:51:41 -0300

    fix lint

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -1,3 +1,3 @@
 db.produtos.updateMany({}, { $set: { valorUnitario: NumberDecimal("0.00") } });
 
-db.produtos.find({}, { _id: 0, nome: 1, curtidas: 1 });
+db.produtos.find({}, { _id: 0, valorUnitario: 1, nome: 1 });

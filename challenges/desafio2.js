77f4e7c6c8cf38a54908e912bc285ef282868f75
guilhermemd <guilherme.md@gmail.com>
2021-08-17 14:28:08 -0300

    feat: :sparkles: req 02 implementation

---
+++ challenges/desafio2.js
@@ -0,0 +1,4 @@
+// 2 - 2 - Inclua o campo valorUnitario em todos os documentos em que esse campo não existe e atribua a ele o valor "0.00", com o tipo NumberDecimal.
+// Crie uma query que retorne o nome e valorUnitario de todos os produtos.
+db.produtos.updateMany({}, { $set: { valorUnitario : NumberDecimal("0.00") } });
+db.produtos.find({}, { valorUnitario: 1, nome: 1, _id: 0 });

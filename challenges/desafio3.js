51954e9fc4c5b262c2f4b27d2c639350151cff95
jonathan <jonathanfernandesjf98@gmail.com>
2021-08-17 17:19:41 -0300

    correcao nome da collection desafio 3

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -1,7 +1,7 @@
-db.products.updateMany({}, { $set: { avaliacao: NumberInt("0") } });
+db.produtos.updateMany({}, { $set: { avaliacao: NumberInt("0") } });
 
-db.products.updateMany({ tags: { $elemMatch: { $eq: "bovino" } } }, { $inc: { avaliacao: 5 } });
+db.produtos.updateMany({ tags: { $elemMatch: { $eq: "bovino" } } }, { $inc: { avaliacao: 5 } });
 
-db.products.updateMany({ tags: { $elemMatch: { $eq: "ave" } } }, { $inc: { avaliacao: 3 } });
+db.produtos.updateMany({ tags: { $elemMatch: { $eq: "ave" } } }, { $inc: { avaliacao: 3 } });
 
-db.products.find({}, { nome: 1, avaliacao: 1, _id: 0 });
+db.produtos.find({}, { nome: 1, avaliacao: 1, _id: 0 });

f30ad2758a516330ac9bf6939e71cc7739211953
Giovanni Maldonado <giovannitadeu08@gmail.com>
2021-08-17 14:48:31 -0300

    feat: desafio 4

---
+++ challenges/desafio4.js
@@ -0,0 +1,8 @@
+db.produtos.updateMany(
+  { nome: "Big Mac" },
+  { $set: { ultimaModificacao: new Date() } },
+);
+db.produtos.find(
+  { ultimaModificacao: { $exists: true } },
+  { _id: 0, nome: 1 },
+);

9e50ea0043c8a66abd44813a7c1372a48f941f8a
Rafael Medeiros <62731714+RafaelMedeirosGomes@users.noreply.github.com>
2021-08-17 16:31:40 -0300

    [new] Implementaçao do requisito 3

---
+++ challenges/desafio3.js
@@ -0,0 +1,4 @@
+db.produtos.updateMany({}, { $set: { avaliacao: NumberInt("0") } });
+db.produtos.updateMany({ tags: { $all: ["bovino"] } }, { $set: { avaliacao: NumberInt("5") } });
+db.produtos.updateMany({ tags: { $all: ["ave"] } }, { $set: { avaliacao: NumberInt("3") } });
+db.produtos.find({}, { nome: 1, avaliacao: 1, _id: 0 }).pretty();

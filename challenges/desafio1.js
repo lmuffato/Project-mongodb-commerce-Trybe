2140f8a3a197658c5f47bfe7c33702141b83edd3
luanlsr <luanfswd@gmail.com>
2021-08-16 23:58:16 -0300

    feat: :sparkles: finished req 6

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -6,4 +6,5 @@ db.produtos.updateMany(
   {},
   { $set: { criadoPor: "Ronald McDonald" } },
 );
+
 db.produtos.find({ criadoPor: "Ronald McDonald" }, { _id: 0, nome: 1, criadoPor: 1 });
--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,1 +1,18 @@
 // 6 - Inclua bacon no final da lista de ingredientes dos sanduíches Big Mac e Quarteirão com Queijo.
+// Crie uma query que faça a inclusão de bacon no final da lista de ingredientes dos sanduíches Big Mac e Quarteirão com Queijo.
+
+// Crie uma query que retorne o nome e ingredientes de todos os documentos.
+db.produtos.updateMany({
+  $or: [
+    { nome: { $eq: "Big Mac" } },
+    { nome: { $eq: "Quarteirão com Queijo" } },
+],
+}, {
+  $addToSet: { ingredientes: "bacon" },
+});
+
+db.produtos.find({}, {
+  _id: 0,
+  nome: 1,
+  ingredientes: 1,
+});

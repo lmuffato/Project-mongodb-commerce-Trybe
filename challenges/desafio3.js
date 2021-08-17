af59bf2715dbd326b457dae591905650778d50a4
ScriptCamilo <script.camilo@gmail.com>
2021-08-17 16:06:45 -0300

    feat: third requirement done

---
+++ challenges/desafio3.js
@@ -0,0 +1,15 @@
+db.produtos.updateMany({}, {
+  $set: { avaliacao: NumberInt(0) },
+});
+
+db.produtos.updateMany(
+  { tags: { $in: ["bovino"] } },
+  { $set: { avaliacao: 5 } },
+);
+
+db.produtos.updateMany(
+  { tags: { $in: ["ave"] } },
+  { $set: { avaliacao: 3 } },
+);
+
+db.produtos.find({}, { _id: 0, nome: 1, avaliacao: 1 });

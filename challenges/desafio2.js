c85b655ee1a699bcc9e03ed72a1fccb3dcab7b1c
Ana Ventura <anaventura1811@gmail.com>
2021-08-17 13:06:40 -0300

    feat: desafio 2

---
+++ challenges/desafio2.js
@@ -0,0 +1,9 @@
+// 2 - Inclua o campo valorUnitario em todos os documentos em que esse campo
+// não existe e atribua a ele o valor "0.00", com o tipo NumberDecimal.
+db.produtos.updateMany({
+valorUnitario: { $exists: false } },
+{ $set: { valorUnitario: NumberDecimal("0.00") } });
+db.produtos.find(
+{ },
+{ nome: 1, valorUnitario: 1, _id: 0 },
+);

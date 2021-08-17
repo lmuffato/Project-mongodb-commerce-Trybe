6efe3c012aaadd1a8986df1fbb28023ab1fa3667
Robertson <robertson.nunes@gmail.com>
2021-08-17 15:15:32 -0300

    Requisito 14

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -7,4 +7,4 @@ db.produtos.updateMany(
       percentual: { $gte: 40 } } } }, { $push: { tags: "muito sódio" } },
 );
 
-db.produtos.find({ }, { nome: 1, tags: 1, _id: 0 }); 
+db.produtos.find({ }, { nome: 1, tags: 1, _id: 0 });

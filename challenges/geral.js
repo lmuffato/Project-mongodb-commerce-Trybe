c4d1ca80447afb10e0d15bca1fcdafe69eb4667c
luanlsr <luanfswd@gmail.com>
2021-08-16 23:58:00 -0300

    feat: :sparkles: finished req 6

---
+++ challenges/geral.js
@@ -0,0 +1,51 @@
+db.produtos.updateMany(
+  {},
+  { $set: { criadoPor: "Ronald McDonald" } },
+);
+
+db.produtos.updateMany(
+  {},
+  { $set: { valorUnitario: NumberDecimal("0.00") } },
+  { upsert: true },
+);
+
+db.produtos.updateMany(
+  {},
+  { $set: { avaliacao: NumberInt(0) } },
+  { upsert: true },
+);
+db.produtos.updateMany(
+  { tags: { $elemMatch: { $eq: "bovino" } } },
+  { $inc: { avaliacao: 5 } },
+);
+db.produtos.updateMany(
+  { tags: { $elemMatch: { $eq: "ave" } } },
+  { $inc: { avaliacao: 3 } },
+);
+
+db.produtos.updateMany({
+  nome: "Big Mac",
+}, {
+    $currentDate: {
+      ultimaModificacao: {
+        $type: "date",
+      },
+    },
+}, {
+  upsert: true,
+});
+
+db.produtos.updateMany({
+  nome: { $ne: "McChicken" },
+}, {
+  $addToSet: { ingredientes: "ketchup" },
+});
+
+db.produtos.updateMany({
+  $or: [
+    { nome: { $eq: "Big Mac" } },
+    { nome: { $eq: "Quarteirão com Queijo" } },
+],
+}, {
+  $addToSet: { ingredientes: "bacon" },
+});

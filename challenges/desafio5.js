b67a55a65a251fce07e5aa9af03d3fcf0b09c855
w-cypriano <profwellington17@gmail.com>
2021-08-17 20:34:11 -0300

    desafio5

---
+++ challenges/desafio5.js
@@ -0,0 +1,20 @@
+db.produtos.updateMany(
+  { 
+    nome: { $ne: "McChicken" },
+  }, 
+  {
+    $addToSet: { 
+      ingredientes: 
+      { 
+        $each: ["ketchup"],
+      },
+    },
+  },
+);
+
+db.produtos.find(
+  {}, 
+  {
+    nome: 1, ingredientes: 1, _id: 0,
+  },
+);

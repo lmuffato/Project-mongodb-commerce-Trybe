b87f04aea98f7ec110abf4eb1c47383b9f94d949
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-08-17 15:06:37 -0300

    Challenge 3

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -1,1 +1,4 @@
 db.produtos.updateMany({}, { $set: { avaliacao: NumberInt(0) } });
+db.produtos.updateMany({ tags: "bovino" }, { $inc: { avaliacao: 5 } });
+db.produtos.updateMany({ tags: "ave" }, { $inc: { avaliacao: 3 } });
+db.produtos.find({}, { avaliacao: true, nome: true, _id: false });

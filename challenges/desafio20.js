b4ceda55c70e20926de79b1a7d9282af37e193d5
luanlsr <luanfswd@gmail.com>
2021-08-17 19:00:08 -0300

    feat: :sparkles: finished req 20

--- challenges/desafio20.js
+++ challenges/desafio20.js
@@ -2,3 +2,5 @@
 // Crie uma query que faça a remoção do campo curtidas do item Big Mac.
 
 // Crie uma query que retorne o nome e curtidas de todos os documentos.
+db.produtos.updateOne({ nome: "Big Mac" }, { $unset: { curtidas: "" } });
+db.produtos.find({}, { _id: 0, nome: 1, curtidas: 1, descricaoSite: 1 });

a301236723746af6e7bc84072101d471fa4b4922
Lucas André <lucas.cancado89@gmail.com>
2021-08-17 11:24:39 -0300

    desafio2:create valorUnitario field

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -5,9 +5,3 @@ db.produtos.updateMany(
   } },
 );
 db.produtos.find({}, { nome: 1, criadoPor: 1, _id: 0 });
-
-// // Crie uma query que adicione o campo criadoPor em todos os 
-// documentos, colocando "Ronald McDonald" no valor desse campo.
-
-// // Crie uma query que retorne o nome e criadoPor de todos os 
-// produtos.

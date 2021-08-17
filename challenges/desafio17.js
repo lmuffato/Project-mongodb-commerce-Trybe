4021436ef51fa7bc1e7f9b2ca43718895dab97ed
Rafael Geronimo <rafaelgeronimo@outlook.com>
2021-08-17 12:05:37 +0000

    feat: challenge 17

---
+++ challenges/desafio17.js
@@ -0,0 +1,10 @@
+// 17 - Conte quantos documentos contêm as palavras frango e
+// hamburguer utilizando o operador $text.
+db.produtos.createIndex(
+    { descricao: "text" },
+    { default_language: "portuguese" },
+);
+
+db.produtos.find(
+    { $text: { $search: "frango hamburguer" } },
+);

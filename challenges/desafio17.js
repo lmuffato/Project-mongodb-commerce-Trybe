5f80b1b2b5b8f56b490dceef8683575bdf12a167
MarcioFujita <hanker78@gmail.com>
2021-08-17 16:58:21 -0300

    Requisitos 16 ao 22

---
+++ challenges/desafio17.js
@@ -0,0 +1,12 @@
+db.produtos.createIndex(
+  { descricao: "text" },
+  { default_language: "portuguese" },
+);
+
+db.produtos.countDocuments(
+  {
+    $text: {
+    $search: "frango hamburguer",
+    },
+  },
+);

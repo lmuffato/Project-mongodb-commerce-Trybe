d5f2f464c103c3e42a27b68e54e60f4b88f948e1
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-08-17 15:20:32 -0300

    Challenge 6

---
+++ challenges/desafio6.js
@@ -0,0 +1,3 @@
+db.produtos.updateMany({ nome: { $in: ["Big Mac", "Quarteirão com Queijo"] } }, 
+{ $push: { ingredientes: "bacon" } });
+db.produtos.find({}, { nome: true, ingredientes: true, _id: false });

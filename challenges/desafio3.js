e7028bd06047acd97101c1e137e7b29ed99fcc6a
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-08-17 16:29:15 -0300

    feat: requisito 3

---
+++ challenges/desafio3.js
@@ -0,0 +1,3 @@
+db.produtos.updateMany({}, { $set: { avaliacao: NumberInt(0) } });
+db.produtos.updateMany({ tags: { $elemMatch: { $eq: "bovino" } } }, { $inc: { avaliacao: 5 } });
+db.produtos.updateMany({ tags: { $elemMatch: { $eq: "ave" } } }, { $inc: { avaliacao: 3 } });

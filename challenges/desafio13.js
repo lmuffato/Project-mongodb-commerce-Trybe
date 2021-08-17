0225bc6740389b041493fde447ed2eb2f63c1b3a
luanlsr <luanfswd@gmail.com>
2021-08-17 18:12:30 -0300

    feat: :sparkles: finished req 13

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,1 +1,26 @@
 // 13 - Adicione o elemento muito sódio ao final do array tags nos produtos em que o percentual de sódio seja maior ou igual a 40.
+// Crie uma query que faça a adição do elemento muito sódio ao final do array tags nos produtos em que o percentual de sódio seja maior ou igual a 40.
+
+// Crie uma query que retorne o nome e tags de todos os documentos.
+db.produtos.updateMany({
+  valoresNutricionais: {
+    $elemMatch: {
+      tipo: {
+        $eq: "sódio",
+      },
+      percentual: {
+        $gte: 40,
+      },
+    },
+  },
+}, {
+  $push: {
+    tags: "muito sódio",
+  },
+});
+
+db.produtos.find({}, {
+  _id: 0,
+  nome: 1,
+  tags: 1,
+});

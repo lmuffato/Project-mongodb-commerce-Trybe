b0b4f3c91cdc50a7297e69e4bb32011c54f44943
RenzoSev <sevilharenzo@gmail.com>
2021-08-17 11:22:52 -0300

    feat: desafio 17

---
+++ desafio17.js
@@ -0,0 +1,6 @@
+// https://docs.mongodb.com/manual/tutorial/specify-language-for-text-index/
+db.produtos.createIndex(
+  { descricao: "text" },
+  { default_language: "portuguese" },
+);
+db.produtos.count({ $text: { $search: "frango hamburguer" } });

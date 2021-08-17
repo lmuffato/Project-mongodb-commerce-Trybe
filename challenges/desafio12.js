c82ff0a1c643fc010e897b3c22cba9d3ad32ae9a
Rafael Geronimo <rafaelgeronimo@outlook.com>
2021-08-17 06:03:05 +0000

    feat: challenge 12

---
+++ challenges/desafio12.js
@@ -0,0 +1,15 @@
+// 12 - Ordene em todos os documentos os elementos do array valoresNutricionais
+// pelo campo percentual de forma descendente.
+db.produtos.updateMany(
+    {},
+    {
+        $push: {
+            valoresNutricionais: {
+                $each: [],
+                $sort: { percentual: -1 },
+            },
+        },
+    },
+);
+
+db.produtos.find({}, { nome: 1, valoresNutricionais: 1, _id: 0 }).pretty();

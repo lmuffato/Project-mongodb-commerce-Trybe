3ff5d78fbe2b38d1deed60121fdf63b33c731495
vinicios <soicinivt@gmail.com>
2021-08-17 17:50:21 -0300

    Desafio 3 - Insert a field avaliacao in all documents and update his values.

---
+++ challenges/desafio3.js
@@ -0,0 +1,32 @@
+db.produtos.updateMany({},
+{
+  $set: {
+    avaliacao: NumberInt(0),
+  },
+},
+{
+  upsert: 1,
+});
+
+db.produtos.updateMany({
+  tags: "bovino",
+},
+{
+  $inc: {
+    avaliacao: 5,
+  },
+});
+
+db.produtos.updateMany({
+  tags: "ave",
+},
+{
+  $inc: {
+    avaliacao: 3,
+  },
+});
+
+db.produtos.find({},
+{
+  nome: 1, avaliacao: 1, _id: 0,
+}).pretty();  

804efaf4b80dc512c8f403a519f2075c6403c138
Mateus Alencar <mtauws@gmail.com>
2021-08-17 15:06:47 -0300

    chore: req 16, 17 and 18

---
+++ challenges/desafio17.js
@@ -0,0 +1,6 @@
+/**
+ * Consultei o repositório do Felipe Pimentel para resolver essa parte.
+ * Link: https://github.com/tryber/sd-09-mongodb-commerce/tree/felipe-pimentel-mongodb-commerce
+ */
+db.produtos.createIndex({ descricao: "text" }, { default_language: "portuguese" });
+db.produtos.count({ $text: { $search: "frango hamburguer" } });

cd62348fd55b20e7c926cc4bdc4309464ab7b736
Eduardo Seije <eduardoseije@gmail.com>
2021-08-16 22:12:23 -0300

    Desafio 11 -  Insira os elementos combo e tasty no array tags de todos os sanduíches e aproveite para deixar os elementos em ordem alfabética ascendente.

---
+++ challenges/desafio10.js
@@ -0,0 +1,14 @@
+db.produtos.updateMany(
+  {},
+  { $set: { vendasPorDia: [0, 0, 0, 0, 0, 0, 0] } },
+);
+
+db.produtos.updateOne(
+  { nome: "Big Mac" },
+  { $inc: { "vendasPorDia[3]": 60 } },
+);
+
+db.produtos.find(
+  {},
+  { nome: 1, vendasPorDia: 1, _id: 0 },
+);

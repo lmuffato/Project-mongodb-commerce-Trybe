70f37797917999c228d35f4d5d6efd939076a633
Rafael Geronimo <rafaelgeronimo@outlook.com>
2021-08-17 04:25:28 +0000

    feat: challenge 8

---
+++ challenges/desafio8.js
@@ -0,0 +1,8 @@
+// 8 - Remova o primeiro ingrediente do sanduíche Quarteirão com Queijo.
+
+db.produtos.updateOne(
+    { nome: "Quarteirão com Queijo" },
+    { $pop: { ingredientes: -1 } },
+);
+
+db.produtos.find({}, { nome: 1, ingredientes: 1, _id: 0 }).pretty();

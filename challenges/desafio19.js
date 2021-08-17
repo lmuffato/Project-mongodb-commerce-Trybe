0571729ec484e6169484c9bd0e80dad367c6409e
Felipe Flores <felipefloresvendas@gmail.com>
2021-08-17 15:23:06 -0300

    feat: desafio 20

--- challenges/desafio19.js
+++ challenges/desafio19.js
@@ -2,4 +2,4 @@ db.produtos.updateMany({},
   { $rename: { descricao: "descricaoSite" },
 });
 
-db.produtos.find({}, { nome: 1, descricao: 1, descricaoSite: 1, _id: 0 }); 
+db.produtos.find({}, { nome: 1, descricao: 1, descricaoSite: 1, _id: 0 });

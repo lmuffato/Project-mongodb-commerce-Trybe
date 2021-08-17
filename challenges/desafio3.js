971039157276f6d42a22afa5dd64f36b5004d27d
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-08-17 09:27:25 -0400

    fix: desafio 3 - lint

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -5,12 +5,12 @@ db.produtos.updateMany(
 
 db.produtos.updateMany(
   { tags: "bovino" },
-  { $inc: { avaliacao: +5 } }
+  { $inc: { avaliacao: +5 } },
 );
 
 db.produtos.updateMany(
   { tags: "ave" },
-  { $inc: { avaliacao: +3 }}
+  { $inc: { avaliacao: +3 } },
 );
 
 db.produtos.find({}, { _id: 0, nome: 1, avaliacao: 1 });

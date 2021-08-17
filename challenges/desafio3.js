d405abf7fc7e42d505c9ff83ba28bbd4e57a94b7
Felipe Muller <felipeavmuller@gmail.com>
2021-08-17 16:15:16 -0300

    Desafio 3 - MongoDB

---
+++ challenges/desafio3.js
@@ -0,0 +1,39 @@
+db.produtos.updateMany(
+  {},
+  { 
+    $set: {
+      avaliacao: NumberInt("0")
+    }
+  }
+);
+
+db.produtos.updateMany(
+  { 
+    tags: {
+      $in: ["bovino"]
+    }
+  },
+  { 
+    $inc: {
+      avaliacao: NumberInt("5")
+    }
+  }
+);
+
+db.produtos.updateMany(
+  { 
+    tags: {
+      $in: ["ave"]
+    }
+  },
+  { 
+    $inc: {
+      avaliacao: NumberInt("3")
+    }
+  }
+);
+
+db.produtos.find(
+  {},
+  { nome: 1, avaliacao: 1, _id: 0 }
+);

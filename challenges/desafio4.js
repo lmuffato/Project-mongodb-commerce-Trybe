e623d7e1f32c6f726ab7a11d239676796e3f2f6a
rogerio-lambert <rogerio.cl@gmail.com>
2021-08-17 17:01:30 -0300

    desafio 4 adiçao do filtro para o campo ultimaModificao existir

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -2,4 +2,7 @@ db.produtos.updateOne(
   { nome: "Big Mac" },
   { $currentDate: { ultimaModificacao: { $type: "date" } } },
 );
-db.produtos.find({}, { nome: 1, ultimaModificacao: 1, _id: 0 });
+db.produtos.find(
+  { ultimaModificacao: { $exists: true } },
+  { nome: 1, ultimaModificacao: 1, _id: 0 },
+);

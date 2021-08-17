922359a824266e97fe3f9d11789386683dd66bb5
Eduardo Seije <eduardoseije@gmail.com>
2021-08-17 09:25:07 -0300

    Desafio 16 - Conte quantos produtos têm 4 ingredientes.

---
+++ challenges/desafio13.js
@@ -0,0 +1,12 @@
+db.produtos.updateMany(
+  { $and: [
+    { "valoresNutricionais.tipo": { $eq: "sódio" } },
+    { "valoresNutricionais.percentual": { $gte: 40 } },
+  ] },
+  { $push: { tags: "muito sódio" } },
+);
+
+db.produtos.find(
+  {},
+  { nome: 1, tags: 1, _id: 0 },
+);

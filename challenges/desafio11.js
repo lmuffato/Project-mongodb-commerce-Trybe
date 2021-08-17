e28978edc12b2d844fc6816c26b94693993b6288
Rafael Geronimo <rafaelgeronimo@outlook.com>
2021-08-17 05:28:01 +0000

    feat: challenge 11

---
+++ challenges/desafio11.js
@@ -0,0 +1,15 @@
+// 11 - Insira os elementos combo e tasty no array tags de todos os sanduíches
+// e aproveite para deixar os elementos em ordem alfabética ascendente.
+db.produtos.updateMany(
+    {},
+    {
+        $push: {
+            tags: {
+                $each: ["combo", "tasty"],
+                $sort: 1,
+            },
+        },
+    },
+);
+
+db.produtos.find({}, { nome: 1, tags: 1, _id: 0 }).pretty();

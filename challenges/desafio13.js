909b80f581d3ca46b4bbb99d24431764d56be488
Parker9309 <juan.tentiomaru9309@gmail.com>
2021-08-18 19:45:04 -0300

    desafio 13 concluído

---
+++ challenges/desafio13.js
@@ -0,0 +1,7 @@
+db.produtos.updateMany(
+  { "valoresNutricionais.percentual": 
+  { $gte: 40 } },
+   { $push: 
+    { tags: "muito sódio" } },
+    );
+db.produtos.find({}, { _id: 0, nome: 1, tags: 1 });

3292c713ff3114e666f9f993f9dec66df9c70e55
Luciano Amâncio <madruga665@hotmail.com>
2021-08-18 02:04:55 -0300

    desafio 19

---
+++ challenges/desafio19.js
@@ -0,0 +1,10 @@
+db.produtos.updateMany({}, {
+  $rename: { descricao: "descricaoSite" },
+});
+
+db.produtos.find({}, {
+  _id: false,
+  nome: true,
+  descricao: true,
+  descricaoSite: true,
+});

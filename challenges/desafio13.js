8a91cfc527cfaa8a4fbd28b505b87dd7723875b0
Luciano Amâncio <madruga665@hotmail.com>
2021-08-17 21:09:03 -0300

    desafio 13

---
+++ challenges/desafio13.js
@@ -0,0 +1,14 @@
+db.produtos.updateMany({ valoresNutricionais: {
+  $elemMatch: { tipo: "sódio", percentual: { $gte: 40 } } } }, {
+  $push: {
+    tags: {
+      $each: ["muito sódio"],
+    },
+  },
+});
+
+db.produtos.find({}, {
+  _id: false,
+  nome: true,
+  tags: true,
+});

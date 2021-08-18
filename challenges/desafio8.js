4ebc55ec5d69f4d7cfb3348de39d5b1e329923e5
Perycles reis <perycles_reis@hotmail.com>
2021-08-18 17:23:00 -0300

    task 8 finished

---
+++ challenges/desafio8.js
@@ -0,0 +1,13 @@
+// 8 - Remova o primeiro ingrediente do sanduíche Quarteirão com Queijo.
+// Para isso, escreva no arquivo desafio8.js duas queries, nesta ordem:
+
+// Crie uma query que faça a remoção do primeiro ingrediente no sanduíche Quarteirão com Queijo.
+
+// Crie uma query que retorne o nome e ingredientes de todos os documentos.
+
+db.produtos.updateMany(
+  { nome: { $eq: "Quarteirão com Queijo" } },
+  { $pop: { ingredientes: -1 } },
+);
+
+db.produtos.find({}, { nome: 1, ingredientes: 1, _id: 0 });

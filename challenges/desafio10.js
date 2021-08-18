4c3e0e02046004e7aeba98145f98b44e78b68ef3
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-08-18 15:37:58 -0300

    Desafio 10

---
+++ challenges/desafio10.js
@@ -0,0 +1,25 @@
+db.produtos.updateMany(
+    {},
+    { $set: {
+      vendasPorDia: [0, 0, 0, 0, 0, 0, 0],
+    } },
+  );
+
+  db.produtos.updateOne(
+    { nome: "Big Mac" },
+    { $set: {
+      "vendasPorDia.3": 60,
+    } },
+);  
+
+db.produtos.updateOne(
+    { tags: { $in: ["bovino", "pão"] } },
+    { $set: {
+      "vendasPorDia.6": 120,
+    } },
+);
+
+db.produtos.find(
+    {},
+    { nome: 1, vendasPorDia: 1, _id: 0 },
+);

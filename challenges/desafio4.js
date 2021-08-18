3dda55cdea62dc2437e3c94f1ace7a652afce4a6
Edrei <edreimateus@gmail.com>
2021-08-17 21:49:19 -0300

    feat: 4 requiriment

---
+++ challenges/desafio4.js
@@ -0,0 +1,14 @@
+db.produtos.updateOne({
+  nome: "Big Mac",
+}, {
+  $currentDate: { ultimaModificacao: { $type: "date" } },
+}, {
+  upsert: true,
+});
+
+db.produtos.find({
+  ultimaModificacao: { $exists: true },
+}, {
+  _id: 0,
+  nome: 1,
+});

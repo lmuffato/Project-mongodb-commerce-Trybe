adc2393db72de44e557eabf2df13d36f160dddf4
Beatriz Barbosa <beatrizcpbarbosa@gmail.com>
2021-08-18 17:44:14 -0300

    requisito 15

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -1,6 +1,6 @@
 db.produtos.updateMany(
-    { "valoresNutricionais.tipo": "sódio", "valoresNutricionais.percentual": { $gte: 40 } },
-    { $push: { tags: "muito sódio" } },
+    { "valoresNutricionais.tipo": "sódio", "valoresNutricionais.percentual": { $gt: 20, $lt: 40 } },
+    { $push: { tags: "contém sódio" } },
 );
 
 db.produtos.find({}, { tags: 1, nome: 1, _id: 0 });

39d24559b854208cf22faf21493bdb315fed6016
w-cypriano <profwellington17@gmail.com>
2021-08-17 21:29:06 -0300

    desafio9

--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -9,3 +9,15 @@ db.produtos.find(
     nome: 1, ingredientes: 1, _id: 0,
   },
 );
+
+db.produtos.updateOne(
+  { nome: "Quarteirão com Queijo" },
+  { $pop: { ingredientes: -1 } },
+);
+
+db.produtos.find(
+  {},
+  {
+    nome: 1, ingredientes: 1, _id: 0,
+  },
+);

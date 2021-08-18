456fdd020468cdcc021806aedd369ef00e77aa94
Marina Alane <marina.alane@outlook.com>
2021-08-18 10:40:24 -0300

    fix lint desafio 12

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -8,9 +8,7 @@ db.produtos.updateMany({},
  valoresNutricionais: {
    $each: [],
    $sort: { percentual: -1 },
- }
-  }}
-  );
+ },
+  } });
  // Crie uma query que retorne o nome e valoresNutricionais de todos os documentos.
  db.produtos.find({}, { nome: 1, valoresNutricionais: 1, _id: 0 });
- 

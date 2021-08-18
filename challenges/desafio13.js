3096f221829b0311de97dd6ce0eae4ad46497cae
Parker9309 <juan.tentiomaru9309@gmail.com>
2021-08-18 20:01:38 -0300

    desafio 14 e 13 fix no filtro e each na projeção

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,7 +1,7 @@
 db.produtos.updateMany(
-  { "valoresNutricionais.percentual": 
+  { "valoresNutricionais.2.percentual": 
   { $gte: 40 } },
    { $push: 
-    { tags: "muito sódio" } },
+    { tags: { each: ["muito sódio"] } } },
     );
 db.produtos.find({}, { _id: 0, nome: 1, tags: 1 });
--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -1,6 +1,6 @@
 db.produtos.updateMany(
-  { "valoresNutricionais.percentual":
+  { "valoresNutricionais.2.percentual":
    { $gt: 20, $lt: 40 } },
-    { $push: { tags: "contém sódio" } },
+    { $push: { tags: { $each: ["contém sódio"] } } },
     );
 db.produtos.find({}, { _id: 0, nome: 1, tags: 1 });

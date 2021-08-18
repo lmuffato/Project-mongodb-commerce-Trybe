0555985896824d0b438cf709ee0cf46bd221d8d5
Parker9309 <juan.tentiomaru9309@gmail.com>
2021-08-18 19:17:07 -0300

    desafio 11 fix

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -1,2 +1,2 @@
 db.produtos.updateMany({}, { $push: { tags: { $each: ["combo", "tasty"], $sort: 1 } } });
-db.produtos.find({}, { _id: 0, nome: 1, vendasPorDia: 1 });
+db.produtos.find({}, { _id: 0, nome: 1, tags: 1 });

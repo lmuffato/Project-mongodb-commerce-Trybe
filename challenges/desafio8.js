a80226776b0ccc42b2ca50476dac353b7d813cbc
biazidioti <bzferreira.bio@gmail.com>
2021-08-19 18:48:47 -0300

    Challenge 9 - Plus ESLint from challenge 8

--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -1,7 +1,7 @@
 // 8 - Remova o primeiro ingrediente do sanduíche Quarteirão com Queijo.
 db.produtos.updateMany( 
-  { nome: "Quarteirão com Queijo"}, 
-  { $pop: { ingredientes: -1 } }
+  { nome: "Quarteirão com Queijo" },
+  { $pop: { ingredientes: -1 } },
   );
 
 db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });
--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -1,1 +1,7 @@
 // 9 - Remova o último ingrediente do sanduíche Cheddar McMelt.
+db.produtos.updateMany(
+  { nome: "Cheddar McMelt" }, 
+  { $pop: { ingredientes: 1 } },
+  );
+
+db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });

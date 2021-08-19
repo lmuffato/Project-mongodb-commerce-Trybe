fabbaa7815472ed734e18caa98db3e041eeecd49
AdelinoJnr <adelinojunior96@gmail.com>
2021-08-19 13:31:15 -0300

    [fix: desafio 17, 18]

--- challenges/desafio17.js
+++ challenges/desafio17.js
@@ -1,3 +1,6 @@
-db.produtos.createIndex({ ingredientes: "text" });
+db.produtos.createIndex(
+  { descricao: "text" },
+  { default_language: "portuguese" },
+);
 
 db.produtos.find({ $text: { $search: "frango hamburguer" } }).count();

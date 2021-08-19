604dd27322986f43603acf5a12a2f16b7fbd4f23
biazidioti <bzferreira.bio@gmail.com>
2021-08-19 18:41:15 -0300

    Challenge 7

--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -1,1 +1,4 @@
 // 7 - Remova o item cebola de todos os sanduíches.
+db.produtos.updateMany({}, { $pull: { ingredientes: "cebola" } });
+
+db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });

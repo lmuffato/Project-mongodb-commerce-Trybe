f01820645a1d95174561a1083af811d56bb351ca
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-08-19 18:26:03 -0300

    agradecimentos no arquivo desafio22

--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -1,1 +1,2 @@
 db.produtos.find({ vendidos: { $mod: [5, 0] } }, { nome: true, vendidos: true, _id: false });
+// agradecimento a Vinicius Rodrigues e iago ferreira no auxilio/referencias durante o projeto

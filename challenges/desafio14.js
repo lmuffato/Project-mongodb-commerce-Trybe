80a7a4bde8ed3659fdc27fdd2d05ba6ec05a6dc8
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-08-19 17:55:26 -0300

    fix: identaçao

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -1,5 +1,16 @@
 db.produtos.updateMany(
-  { valoresNutricionais: { $elemMatch: { tipo: "sódio", percentual: { $gt: 20, $lt: 40 } } } }, 
-  { $push: { tags: "comtém sódio" } },
+  {
+    valoresNutricionais:
+    { $elemMatch:
+      { tipo: "sódio",
+      percentual: {
+        $gt: 20,
+        $lt: 40,
+      },
+    },
+  },
+},
+{ $push: { tags: "comtém sódio" } },
 );
+
 db.produtos.find({}, { nome: true, tags: true, _id: false });

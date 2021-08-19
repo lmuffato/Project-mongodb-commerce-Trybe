21efff02511cb948a9e31022da72b33c90579d33
biazidioti <bzferreira.bio@gmail.com>
2021-08-19 16:00:46 -0300

    Challenge 3

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -1,1 +1,7 @@
 // 3 - Adicione o campo avaliacao em todos os documentos da coleção e efetue alterações nesse campo.
+db.produtos.updateMany({}, { $set: { avaliacao: NumberInt("0") } });
+
+db.produtos.updateMany({ tags: "bovino"}, { $inc: { avaliacao: +5 } });
+db.produtos.updateMany({ tags: "ave"}, { $inc: { avaliacao: +3 } });
+
+db.produtos.find({}, { _id: 0, nome: 1, avaliacao: 1 });

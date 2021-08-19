b2a5bc8fd519da7c9eeb3ceaad6666fa0078005a
biazidioti <bzferreira.bio@gmail.com>
2021-08-19 19:23:00 -0300

    Challenge 17

--- challenges/desafio17.js
+++ challenges/desafio17.js
@@ -1,1 +1,7 @@
 // 17 - Conte quantos documentos contêm as palavras frango e hamburguer utilizando o operador $text.
+db.produtos.createIndex(
+  { descricao: "text" }, 
+  { default_language: "portuguese" },
+  );
+
+db.produtos.find({ $text: { $search: "frango hamburguer" } }).count();

5973606844ed3a2d0fe65366f8c90e609f2bc472
Gabriel Ferreira <glucas709@gmail.com>
2021-08-19 15:35:16 -0300

    Challenge 4

---
+++ challenges/desafio4.js
@@ -0,0 +1,14 @@
+use("commerce");
+
+// db.produtos.find();
+
+db.produtos.updateOne(
+  { nome: "Big Mac" },
+  {
+    $set: {
+      ultimaModificacao: new Date(),
+    },
+  },
+);
+
+db.produtos.find({}, { _id: 0, nome: 1, ultimaModificacao: 1 });

ae0ae6b11448ec57823cc3d4aa2797334d301832
jlfagundes <jlfagundes@gmail.com>
2021-08-18 21:58:00 -0300

    fix: lint challente 5 and 6

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,6 +1,6 @@
 db.produtos.updateMany(
   { nome: { $in: ["Big Mac", "Quarteirão com Queijo"] } },
-  { $push: { ingredientes: "bacon" } }
+  { $push: { ingredientes: "bacon" } },
 );
 
-db.produtos.find({}, { nome: 1, ingredientes: 1, _id: 0});
+db.produtos.find({}, { nome: 1, ingredientes: 1, _id: 0 });

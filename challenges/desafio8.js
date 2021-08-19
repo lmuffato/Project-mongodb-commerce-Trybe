60650fbcdcbe549cdc9dea1534ae9c6ee331663e
Eduardo Costa Maciel <costaeduardomaciel@gmail.com>
2021-08-19 19:28:20 -0300

    Feat: (req-8) Remove the first ingredient from the Block with Cheese sandwich.

---
+++ challenges/desafio8.js
@@ -0,0 +1,16 @@
+// use ("commerce");
+// db.produtos.find();
+
+db.produtos.updateOne(
+  { nome: "Quarteirão com Queijo" },
+  { $pop: { ingredientes: -1 } },
+);
+
+db.produtos.find(
+  {},
+  {
+    _id: 0,
+    nome: 1,
+    ingredientes: 1,
+  },
+);

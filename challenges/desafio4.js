80cfcf6836642e90574e0a49b7e226bb377e1b82
Antônio Schappo <antonioschappo@gmail.com>
2021-08-19 15:45:25 -0300

    first try challenge 4

---
+++ challenges/desafio4.js
@@ -0,0 +1,6 @@
+db.produtos.updateMany({ nome: "Big Mac" },
+    { $set: { $currentDate: { ultimaModificacao: { $type: "timestamp" } } } });
+
+db.produtos.find({ ultimaModificacao: { $exists: true } },
+    { _id: 0, nome: 1, ultimaModificacao: 1 });
+    

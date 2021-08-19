8f52f82c99762d6c6fdd2da4796f49db58bf0e85
biazidioti <bzferreira.bio@gmail.com>
2021-08-19 18:59:28 -0300

    Challenge 10

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,1 +1,15 @@
 // 10 - Adicione a quantidade de vendas dos sanduíches por dia da semana.
+db.produtos.updateMany({}, 
+  { $set: { vendasPorDia: [0, 0, 0, 0, 0, 0, 0] } });
+  
+db.produtos.updateOne(
+  { nome: "Big Mac" },
+  { $inc: { "vendasPorDia.3": 60 } },
+  );
+
+db.produtos.updateMany(
+{ tags: { $all: ["bovino", "pão"] } },
+{ $inc: { "vendasPorDia.6": 120 } },
+);
+
+db.produtos.find({}, { _id: 0, nome: 1, vendasPorDia: 1 });

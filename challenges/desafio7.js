08f7a07ba7daa6b493acd4c563759d9001b4be4c
Eduardo Costa Maciel <costaeduardomaciel@gmail.com>
2021-08-19 19:20:44 -0300

    Feat: (req-7) Remove the onion item from all sandwiches.

---
+++ challenges/desafio7.js
@@ -0,0 +1,16 @@
+// use ("commerce");
+// db.produtos.find();
+
+db.produtos.updateMany(
+  {},
+  { $pull: { ingredientes: "cebola" } },
+);
+
+db.produtos.find(
+  {},
+  {
+    _id: 0,
+    nome: 1,
+    ingredientes: 1,
+  },
+);

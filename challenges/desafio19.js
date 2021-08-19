65bd1279c2b440f5fc868bd0d6e629022767c9bc
Perycles reis <perycles_reis@hotmail.com>
2021-08-18 22:29:58 -0300

    task 19 finished refact

--- challenges/desafio19.js
+++ challenges/desafio19.js
@@ -7,7 +7,7 @@
 
 db.produtos.updateMany(
   { },
-  { $rename: { "descricao": "descricaoSite" } },
+  { $rename: { descricao: "descricaoSite" } },
 );
 
 db.produtos.find({}, { nome: 1, descricaoSite: 1, _id: 0 });

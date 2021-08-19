ee2a334faf0bb60511df87bec62ce9a7cafe2683
viniciusfranca019 <viniciusfrancas019@gmail.com>
2021-08-18 22:11:12 -0300

    req 17 done

---
+++ challenges/desafio17.js
@@ -0,0 +1,13 @@
+// info do default_lang tirada do seguinte link:
+// https://docs.mongodb.com/manual/tutorial/specify-language-for-text-index/
+
+db.produtos.createIndex(
+  { descricao: "text" },
+  { default_language: "portuguese" }
+  );
+
+db.produtos.find(
+  {
+    $text: { $search: "frango hamburguer" },
+  }
+).count();

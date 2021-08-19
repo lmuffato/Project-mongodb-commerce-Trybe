962576228fe325b59f3307b1d1f20d1c2f6c389d
Antônio Schappo <antonioschappo@gmail.com>
2021-08-19 14:51:52 -0300

    first try challenge 3

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -1,11 +1,7 @@
-// ### 3 - Adicione o campo `avaliacao` em todos os documentos da coleção e efetue alterações nesse campo.
+db.produtos.updateMany({}, { $set: { avaliacao: NumberInt("0") } });
 
-// Para isso, escreva no arquivo `desafio3.js` quatro queries, **nesta ordem**:
+db.produtos.updateMany({ tags: "bovino" }, { $inc: { avaliacao: 5 } });
 
-// 1. Crie uma query que inclua o campo `avaliacao` do tipo `NumberInt` e com o valor `0` em todos os documentos da coleção.
+db.produtos.updateMany({ tags: "ave" }, { $inc: { avaliacao: 3 } });
 
-// 2. Crie uma query que incremente o valor do campo `avaliacao` em `5` em todos os sanduíches de carne do tipo `bovino`. Dica: utilize como filtro o campo `tags`.
-
-// 3. Crie uma query que incremente o valor do campo `avaliacao` em `3` em todos os sanduíches de `ave`.
-
-// 4. Crie uma query que retorne o `nome` e `avaliacao` de todos os sanduíches.
+db.produtos.find({}, { _id: 0, nome: 1, avaliacao: 1 });

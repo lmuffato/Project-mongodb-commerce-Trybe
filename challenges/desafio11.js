a9e0befbc20434b9ecc4db40539fa12189db196e
biazidioti <bzferreira.bio@gmail.com>
2021-08-19 19:05:31 -0300

    Challenge 11

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -1,1 +1,5 @@
 // 11 - Insira os elementos combo e tasty no array tags de todos os sanduíches e aproveite para deixar os elementos em ordem alfabética ascendente.
+db.produtos.updateMany({},
+  { $push: { tags: { $each: ["combo", "tasty"], $sort: 1 } } });
+
+db.produtos.find({}, { _id: 0, nome: 1, tags: 1 });

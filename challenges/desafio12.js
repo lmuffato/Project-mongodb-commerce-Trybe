aa4985d9de28480e88152a5fa233d7bf7b9140a7
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-08-19 17:00:48 -0300

    feat: requisito 12

---
+++ challenges/desafio12.js
@@ -0,0 +1,4 @@
+db.produtos.updateMany(
+  {}, { $push: { valoresNutricionais: { $each: [], $sort: { percentual: -1 } } } },
+);
+db.produtos.find({}, { nome: true, valoresNutricionais: true, _id: false });

ee67921c8bebdff77efb5e1f2264e3fe783bd7cc
Eduardo Costa Maciel <costaeduardomaciel@gmail.com>
2021-08-19 10:05:35 -0300

    Feat: (req-5) Add ketchup to the ingredients for all sandwiches but McChicken, ensuring there is no duplication of ingredients

---
+++ challenges/desafio5.js
@@ -0,0 +1,22 @@
+// use ("commerce");
+// db.produtos.find(
+//   { nome: { $ne: "McChicken" } }
+// );
+
+db.produtos.updateMany(
+  { nome: { $ne: "McChicken" } },
+  {
+    $addToSet: {
+      ingredientes: "ketchup",
+    },
+  },
+);
+
+db.produtos.find(
+  {},
+  {
+    _id: 0,
+    nome: 1,
+    ingredientes: 1,
+  },
+);

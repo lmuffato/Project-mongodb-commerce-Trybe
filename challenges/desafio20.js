406a77c18a29fa4cb1b320968ef0b257ecdb9bbd
biazidioti <bzferreira.bio@gmail.com>
2021-08-19 19:26:05 -0300

    Challenge 20

--- challenges/desafio20.js
+++ challenges/desafio20.js
@@ -1,1 +1,7 @@
 // 20 - Remova o campo curtidas do item Big Mac.
+db.produtos.updateOne(
+  { nome: "Big Mac" },
+  { $unset: { curtidas: "" } },
+);
+
+db.produtos.find({}, { _id: 0, nome: 1, curtidas: 1 });

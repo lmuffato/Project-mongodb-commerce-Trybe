d28e6120fd90c9156e958dbb6f76610b4c23d66f
Vinicius Rodrigues <v.rodriguesjk@gmail.com>
2021-08-19 10:50:45 -0300

    Fix: Desafio 10

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -5,12 +5,12 @@ db.produtos.updateMany(
 
 db.produtos.updateOne(
   { nome: "Big Mac" },
-  { $inc: { "$vendasPorDia.3": 60 } },
+  { $inc: { "vendasPorDia.3": 60 } },
 );
 
 db.produtos.updateOne(
   { tags: { $in: ["bovina", "pão"] } },
-  { $inc: { "$vendasPorDia.6": 120 } },
+  { $inc: { "vendasPorDia.6": 120 } },
 );
 
 db.produtos.find({ }, { nome: 1, vendasPorDia: 1, _id: 0 });

4d50e37c8db2fab6681fbf385d185141d8472cd5
biazidioti <bzferreira.bio@gmail.com>
2021-08-19 16:13:12 -0300

    Challenge 4

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,1 +1,6 @@
 // 4 - Atribua a data corrente ao campo ultimaModificacao no sanduíche Big Mac.
+db.produtos.updateOne(
+  { nome: "Big Mac" }, { $currentDate: { ultimaModificacao: { $type: "date" } } },
+  );
+
+db.produtos.find({}, { _id: 0, nome: 1, ultimaModificacao: 1 });

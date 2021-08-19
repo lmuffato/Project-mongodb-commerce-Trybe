4b062b598d04fa4c471faf0d276df51906f30032
biazidioti <bzferreira.bio@gmail.com>
2021-08-19 18:32:44 -0300

    Challenge 5

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,1 +1,6 @@
 // 5 - Adicione ketchup aos ingredientes para todos os sanduíches menos o McChicken, garantindo que não haja duplicidade nos ingredientes.
+db.produtos.updateMany(
+  { nome: { $ne: "McChicken" } }, 
+  { $addToSet: { ingredientes: "ketchup" } },
+  );
+db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });

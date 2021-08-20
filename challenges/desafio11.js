37bcdd77e1e8bc84a15b2c1da884a394d01cfd5d
lucas-chave <lcchavesr150@gmail.com>
2021-08-20 08:54:13 -0300

    fix challenge 10

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -1,3 +1,8 @@
 // challenge 11;
 
-// db.produtos.updateMany({}, { $push: { tags: { $each: ["combo", "tasty"] } }, $sort: 1 });
+db.produtos.updateMany({}, { 
+  $push: { tags: { $each: ["combo", "tasty"], $sort: 1 } },
+  
+});
+
+db.produtos.find({}, { nome: 1, tags: 1, _id: 0 });

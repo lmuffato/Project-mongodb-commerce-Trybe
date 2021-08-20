1b49712ebb55d362128e2bc1218163a8deace97b
lucas-chave <lcchavesr150@gmail.com>
2021-08-20 08:28:10 -0300

    fix challenge 10

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -5,7 +5,7 @@ db.produtos.updateMany({}, { $push: { vendasPorDia: { $each: [0, 0, 0, 0, 0, 0, 
 db.produtos.updateOne({ nome: "Big Mac" }, { $inc: { "vendasPorDia.3": 60 } });
 
 db.produtos.updateMany(
-  { $and: [{ tags: ["bovino", "pão"] }] }, { $inc: { "vendasPorDia.6": 120 } },
+  { tags: ["bovino", "pão"] }, { $inc: { "vendasPorDia.6": 120 } },
 );
 
 db.produtos.find({}, { nome: 1, vendasPorDia: 1, _id: 0 });
---
+++ challenges/desafio11.js
@@ -0,0 +1,3 @@
+// challenge 11;
+
+// db.produtos.updateMany({}, { $push: { tags: { $each: ["combo", "tasty"] } }, $sort: 1 });

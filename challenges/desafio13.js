81531e51ff2876a6c1e40c1bbd3bec64f8782379
Eduardo Costa Maciel <costaeduardomaciel@gmail.com>
2021-08-20 13:25:36 -0300

    Feat: (req-13) Add the high sodium element to the end of the tags array in products where the sodium percentage is greater than or equal to 40.

---
+++ challenges/desafio13.js
@@ -0,0 +1,27 @@
+// use ("commerce");
+// db.produtos.find();
+
+db.produtos.updateMany(
+  {
+    valoresNutricionais: {
+      $elemMatch: {
+        tipo: "sódio",
+        percentual: { $gte: 40 },
+      },
+    },
+  },
+  {
+    $push: {
+      tags: "muito sódio",
+    },
+  },
+);
+
+db.produtos.find(
+  {},
+  {
+    _id: 0,
+    nome: 1,
+    tags: 1,
+  },
+);

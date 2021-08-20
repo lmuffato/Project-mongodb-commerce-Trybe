60d57bf0bbd429401ed627c343af5a52b643696d
andy silva <andssilva.23@gmail.com>
2021-08-20 17:22:14 -0400

    feat(req 2): :rocket: challenge 2

---
+++ challenges/desafio2.js
@@ -0,0 +1,13 @@
+// forma de construir códigos usando tipo de dados encontrado no link:
+// https://docs.huihoo.com/mongodb/3.4/core/shell-types/index.html
+
+// use("commerce")
+db.produtos.updateMany(
+  { valorUnitario: { $exists: false } },
+  { $set: { valorUnitario: NumberDecimal("0.00") } },
+);
+
+db.produtos.find(
+  {},
+  { _id: 0, nome: 1, valorUnitario: 1 },
+);

73d0892a52928abcf3424f108c0ce7f192fa28e9
Paulo Vitor Borges <pvtrybe209@gmail.com>
2021-08-20 15:28:49 -0300

    fix:desafio5

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,4 +1,4 @@
 db.produtos
 .updateMany({ nome: { $ne: "McChicken" } },
- { $push: { ingredientes: "ketchup" } });
+ { $addToSet: { ingredientes: "ketchup" } });
  db.produtos.find({}, { nome: 1, ingredientes: 1, _id: 0 });

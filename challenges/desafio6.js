6103b80ff6bb7590eb49a3c56a9756f7eb7cd422
Paulo Vitor Borges <pvtrybe209@gmail.com>
2021-08-20 15:02:02 -0300

    desafio6

---
+++ challenges/desafio6.js
@@ -0,0 +1,4 @@
+db.produtos
+.updateMany({ nome: { $in: ["Big Mac", "Quarteirão com Queijo"] } },
+ { $push: { ingredientes: "bacon" } }, { upsert: true });
+ db.produtos.find({}, { nome: 1, ingredientes: 1, _id: 0 });

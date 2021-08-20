136025d47b59ace202cefa82c7d5b436d701a0fd
andy silva <andssilva.23@gmail.com>
2021-08-20 17:43:43 -0400

    feat(req 4): :rocket: challenge 4

---
+++ challenges/desafio4.js
@@ -0,0 +1,12 @@
+// use("commerce")
+db.produtos.updateOne(
+  { nome: "Big Mac" },
+  { $currentDate: {
+    ultimaModificacao: { $type: "date" },
+  } },
+);
+
+db.produtos.find(
+  { ultimaModificacao: { $exists: true } },
+  { _id: 0, nome: 1 },
+);

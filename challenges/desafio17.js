3098d36b15b8d7c7e2d19472744cc286c1ec569c
lucas-chave <lcchavesr150@gmail.com>
2021-08-20 10:27:43 -0300

    challenge 17

--- challenges/desafio17.js
+++ challenges/desafio17.js
@@ -1,3 +1,5 @@
 // challenge 17;
 
-// db.produtos.createIndex({ descricao: "text" });
+db.produtos.createIndex({ descricao: "text" });
+
+db.produtos.find({ $text: { $search: "frango hamburguer" } }).count();

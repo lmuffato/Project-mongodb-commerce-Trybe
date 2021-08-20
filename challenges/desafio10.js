e434a1f725eaede8be727f70acff0a940b43b20c
Stanrley Barbosa <3011stan@gmail.com>
2021-08-19 23:25:28 -0300

    Fix lint

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -6,16 +6,15 @@ db.produtos.updateMany( // 1
 );
 // 2 - Crie uma query que incremente as vendas de Big Mac às quartas-feiras em 60.
 db.produtos.updateOne(
-  { nome: "Big Mac"},
+  { nome: "Big Mac" },
   { $set: { "vendasPorDia.3": 60 } },
 );
 // 3 - Crie uma query que incremente as vendas de todos os sanduíches de carne do tipo bovino e pão aos sábados em 120.
 db.produtos.updateOne(
-  { tags: { $in: ["bovino", "pão"]} },
+  { tags: { $in: ["bovino", "pão"] } },
   { $set: { "vendasPorDia.6": 120 } },
 );
 db.produtos.find( // 4
   { },
   { _id: 0, nome: 1, vendasPorDia: 1 },
 );
-

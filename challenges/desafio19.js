0d0dff52351a1a754e2800326d2ff4eadf40f06e
Stanrley Barbosa <3011stan@gmail.com>
2021-08-20 21:11:07 -0300

    Req 19 check

---
+++ challenges/desafio19.js
@@ -0,0 +1,12 @@
+// 19 - Renomeie o campo descricao para descricaoSite em todos os documentos.
+// use("commerce");
+db.produtos.updateMany(
+  { },
+  {
+    $rename: { descricao: "descricaoSite" },
+  },
+);
+db.produtos.find(
+  { },
+  { _id: 0, nome: 1, descricao: 1, descricaoSite: 1 },
+);

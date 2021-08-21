5b4102735e8281bc4eb193246da8aab1e99bedf2
andy silva <andssilva.23@gmail.com>
2021-08-21 11:51:46 -0400

    feat(req 20): :rocket: challenge 20

---
+++ challenges/desafio20.js
@@ -0,0 +1,12 @@
+// use("commerce")
+db.produtos.updateOne(
+  { nome: "Big Mac" },
+  { $unset: {
+    curtidas: "",
+  } },
+);
+
+db.produtos.find(
+  {},
+  { _id: 0, nome: 1, curtidas: 1 },
+);

56af2cfb68927b192de59aa787e04da4d4d015b7
Stanrley Barbosa <3011stan@gmail.com>
2021-08-20 21:15:26 -0300

    Req 20 check

---
+++ challenges/desafio20.js
@@ -0,0 +1,10 @@
+// 20 - Remova o campo curtidas do item Big Mac.
+// use("commerce");
+db.produtos.updateMany(
+  { nome: "Big Mac" },
+  { $unset: { curtidas: "" } },
+);
+db.produtos.find(
+  { },
+  { _id: 0, nome: 1, curtidas: 1 },
+);

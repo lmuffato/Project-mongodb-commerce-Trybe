3393bdfbfac51dd327e4dda20c97842daa73e37d
William <w.willers@outlook.com>
2021-08-22 18:43:05 -0400

    Correcao nome do desafio2, acrescimo desafio 3 e 10 - que ja estavam prontos -

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -1,23 +1,8 @@
-db.produtos.updateMany(
-  {},
-  { $set: { vendasPorDia: [0, 0, 0, 0, 0, 0, 0] } },
-);
-
-db.produtos.updateOne(
-  { nome: "Big Mac" },
-  {
-    $inc: { "vendasPorDia.3": 60 }, 
-  },
-);
-
-db.produtos.updateMany(
-  { tags: {
-    $in: ["bovino", "pão"],
-  } },
-  { $inc: { "vendasPorDia.6": 120 } },
-);
-
 db.produtos.find(
-  {},
-  { _id: 0, nome: 1, vendasPorDia: 1 },
+  { 
+    vendidos: {
+      $mod: [5, 0],
+    },
+  },
+  { _id: 0, nome: 1, vendidos: 1 },
 );

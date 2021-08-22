826e62a8f1c4f75e53a0389e038fb44fe71d0191
Erick-Marinho <erickgaves@gmail.com>
2021-08-22 19:55:02 -0300

    Cumprindo desafio 2

---
+++ challenges/desafio2.js
@@ -0,0 +1,24 @@
+const query = {};
+
+const update = {
+  $set: { 
+    valorUnitario: NumberDecimal("0.00"),
+  },
+};
+
+const params = {
+  upsert: true,
+  multi: true,
+};
+
+const projectionFind = {
+  nome: 1, valorUnitario: 1, _id: 0,
+};
+
+db.produtos.update(
+  query,
+  update,
+  params,
+);
+
+db.produtos.find(query, projectionFind);

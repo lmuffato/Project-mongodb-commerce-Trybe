a0d30bb31f1331b7c8335c876e08334962d66b16
IzeldaValeriano <zel.valeriano@gmail.com>
2021-08-22 11:12:45 -0300

    corrigindo erros lint

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -2,5 +2,4 @@ db.produtos.updateMany(
     {},
     { $push: { valoresNutricionais: { $each: [], $sort: { percentual: -1 } } } },
   );
-  db.produtos.find({}, { _id: false, nome: true, valoresNutricionais: true });
-  
+db.produtos.find({}, { _id: false, nome: true, valoresNutricionais: true });
--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -3,5 +3,4 @@ db.produtos.updateMany(
     { $set: { valorUnitario: NumberDecimal("0.00") } },
   );
   
-  db.produtos.find({}, { _id: false, nome: true, valorUnitario: true });
-  
+db.produtos.find({}, { _id: false, nome: true, valorUnitario: true });

62e0452dd8726724e376649ed66cb9f709cc2685
Erick-Marinho <erickgaves@gmail.com>
2021-08-23 11:17:28 -0300

    Refatorando codigo

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -1,62 +1,19 @@
-const queryAll = {};
-
-const queryBovino = {
-  "tags.0": { $eq: "bovino" },
-};
-
-const increBovino = {
-  $inc: {
-    avaliacao: 5,
-  },
-};
-
-const queryAve = {
-  "tags.0": { $eq: "ave" },
-};
-
-const increAve = {
-  $inc: {
-    avaliacao: 3,
-  },
-};
-
-const updateAvaliacao = {
-  $set: {
-    avaliacao: NumberInt("0"),
-  },
-};
-
-const params = {
-  upsert: true,
-  multi: true,
-};
-
-const projectionFind = {
-  nome: 1,
-  avaliação: 1,
-  _id: 0,
-};
-
-// 1. Inserção campos avaliacao
 db.produtos.updateMany(
-  queryAll,
-  updateAvaliacao,
-  params,
+  {},
+  { $set: { avaliacao: NumberInt("0") } },
+  { upsert: true, multi: true },
 );
 
-// 2. Incrementado avaliação sanduíche tipo bovino
 db.produtos.updateMany(
-  queryBovino,
-  increBovino,
-  params,
+  { "tags.0": { $eq: "bovino" } },
+  { $inc: { avaliacao: 5 } },
+  { upsert: true, multi: true },
 );
 
-// 3. Incrementando avaliação sanduícke tipo Ave
 db.produtos.updateMany(
-  queryAve,
-  increAve,
-  params,
+  { tags: { $eq: "ave" } },
+  { $inc: { avaliacao: 3 } },
+  { upsert: true, multi: true },
 );
 
-// 4. Retorno dos campos pedidos
-db.produtos.find(queryAll, projectionFind);
+db.produtos.find({}, { _id: 0, nome: 1, avaliacao: 1 });

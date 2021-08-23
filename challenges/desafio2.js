7d3ceaf88b8de2713508964bc2dad74060711171
Renata Andrade <renata@MacBook-Air-de-Renata.local>
2021-08-23 15:15:42 -0300

    fix: desafio 2 - corrige tipo para NumberDecimal

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -1,9 +1,11 @@
 db.produtos.updateMany(
   { valorUnitario: { $exists: false } },
-  { $set: { valorUnitario: "0.00" } },
+  { $set: { valorUnitario: NumberDecimal("0.00") } },
 );
 
 db.produtos.find(
   {}, 
   { nome: 1, valorUnitario: 1, _id: 0 },
 );
+
+/* Como indicar tipo NumberDecimal: https://docs.mongodb.com/manual/core/shell-types/ */

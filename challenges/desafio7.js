b501a50de5ca2e383f9276325e1e1939c4992905
Erick-Marinho <erickgaves@gmail.com>
2021-08-23 17:21:04 -0300

    cumprindo desafio 7

---
+++ challenges/desafio7.js
@@ -0,0 +1,24 @@
+const queryAll = {};
+
+const params = {
+  upsert: true,
+  multi: true,
+};
+
+const projectionFind = {
+  nome: 1, 
+  ingredientes: 1, 
+  _id: 0,
+};
+
+db.produtos.updateMany(
+  queryAll,
+  { $pull: {
+    ingredientes: {
+      $in: ["cebola"],
+    },
+  } },
+  params,
+);
+
+db.produtos.find(queryAll, projectionFind);

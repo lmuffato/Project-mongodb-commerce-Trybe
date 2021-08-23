171ea363ebd1b21b5d61ebb4cc760b31b1098681
Erick-Marinho <erickgaves@gmail.com>
2021-08-23 15:19:27 -0300

    cumprindo desafio 5

---
+++ challenges/desafio5.js
@@ -0,0 +1,18 @@
+const projectionFind = {
+  nome: 1, 
+  ingredientes: 1, 
+  _id: 0,
+};
+
+const params = {
+  upsert: true,
+  multi: true,
+};
+
+db.products.findOneAndUpdate(
+  { nome: { $nin: "McChiken" } },
+  { $addToSet: { ingredientes: "ketchup" } },
+  params,
+);
+
+db.produtos.find({}, projectionFind);

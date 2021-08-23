f04efc3482ffa15dc1d6befdebbc055788c0bd83
Erick-Marinho <erickgaves@gmail.com>
2021-08-23 12:52:38 -0300

    cumprindo desafio 4

---
+++ challenges/desafio4.js
@@ -0,0 +1,12 @@
+db.produtos.findOneAndUpdate(
+  { nome: "Big Mac" },
+  { $currentDate: {
+      ultimaModificacao: true,
+    },
+  },
+);
+
+db.produtos.find(
+  { ultimaModificacao: { exists: true } },
+  { _id: 0, nome: 1 },
+);

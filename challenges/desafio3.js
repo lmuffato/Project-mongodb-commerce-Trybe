e990e2ac2479c1536f8912fbef826740f7026334
Renata Andrade <renata@MacBook-Air-de-Renata.local>
2021-08-23 14:24:06 -0300

    feat: desafio 3

---
+++ challenges/desafio3.js
@@ -0,0 +1,19 @@
+db.produtos.updateMany(
+  { },
+  { $set: { avaliacao: 0 } },
+);
+
+db.produtos.updateMany(
+  { tags: { $elemMatch: { $eq: "bovino" } } },
+  { $inc: { avaliacao: 5 } },
+);
+
+db.produtos.updateMany(
+  { tags: { $elemMatch: { $eq: "ave" } } },
+  { $inc: { avaliacao: 3 } },
+);
+
+db.produtos.find(
+  {}, 
+  { nome: 1, avaliacao: 1, _id: 0 },
+);

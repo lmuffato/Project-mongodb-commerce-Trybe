9b22301ca4a0c82da434f439bd3046b7031abf2a
Junior Portela <portelajr14@gmail.com>
2021-08-23 16:30:52 -0300

    feat: desafio 14 feito

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,12 +1,13 @@
 db.produtos.updateMany(
-  { valoresNutricionais: 
-    { $elemMatch: 
-      { tipo: "sódio", percentual: { $gte: 40 } },
+  {
+    valoresNutricionais: 
+    { 
+      $elemMatch: 
+      { tipo: "sódio",
+        percentual: { $gte: 40 },
+      },
     },
   },
-  { $push: {
-      tags: "muito sódio",
-    },
-  },
+  { $push: { tags: "muito sódio" } },
 );
 db.produtos.find({}, { nome: 1, tags: 1, _id: 0 });

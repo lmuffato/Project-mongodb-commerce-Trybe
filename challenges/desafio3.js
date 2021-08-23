fd43f811dd28a3b1ddf9bdb23b7c9ae29d1eac9d
Erick-Marinho <erickgaves@gmail.com>
2021-08-23 12:03:22 -0300

    Refatorando, exclusão de parâmetros

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -1,19 +1,16 @@
 db.produtos.updateMany(
   {},
   { $set: { avaliacao: NumberInt("0") } },
-  { upsert: true, multi: true },
 );
 
 db.produtos.updateMany(
   { "tags.0": { $eq: "bovino" } },
   { $inc: { avaliacao: 5 } },
-  { upsert: true, multi: true },
 );
 
 db.produtos.updateMany(
   { tags: { $eq: "ave" } },
   { $inc: { avaliacao: 3 } },
-  { upsert: true, multi: true },
 );
 
 db.produtos.find({}, { _id: 0, nome: 1, avaliacao: 1 });

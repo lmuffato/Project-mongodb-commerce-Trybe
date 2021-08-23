05fe701379cbe3aa0dafbdfee2f17a497ad792b7
Erick-Marinho <erickgaves@gmail.com>
2021-08-23 19:27:18 -0300

    cumprindo desafio 10

---
+++ challenges/desafio10.js
@@ -0,0 +1,40 @@
+const queryAll = {};
+
+const params = {
+  upsert: true,
+  multi: true,
+};
+
+const projectionFind = {
+  nome: 1, 
+  vendasPorDia: 1, 
+  _id: 0,
+};
+
+db.produtos.updateMany(
+  {},
+  { $push: {
+    vendasPorDia: {
+      $each: [0, 0, 0, 0, 0, 0, 0],
+    },
+  } },
+  params,
+);
+
+db.produtos.findOneAndUpdate(
+  { nome: "Big Mac" },
+  { $inc: {
+    "vendasPorDia.3": 60,
+  } },
+  params,
+);
+
+db.produtos.updateMany(
+  { tags: { $in: ["bovino", "pao"] } },
+  { $inc: { 
+    "vendasPorDia.6": 120,
+  } },
+  params,
+);
+
+db.produtos.find(queryAll, projectionFind);

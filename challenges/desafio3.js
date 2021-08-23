3f7325c0a286c45a902c6e9a94c28089bcca9bff
Erick-Marinho <erickgaves@gmail.com>
2021-08-23 12:27:29 -0300

    incluindo parametros

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -1,16 +1,24 @@
+const params = {
+  upsert: true,
+  multi: true,
+};
+
 db.produtos.updateMany(
   {},
   { $set: { avaliacao: NumberInt("0") } },
+  params,
 );
 
 db.produtos.updateMany(
   { "tags.0": { $eq: "bovino" } },
   { $inc: { avaliacao: 5 } },
+  params,
 );
 
 db.produtos.updateMany(
   { "tags.0": { $eq: "ave" } },
   { $inc: { avaliacao: 3 } },
+  params,
 );
 
 db.produtos.find({}, { _id: 0, nome: 1, avaliacao: 1 });

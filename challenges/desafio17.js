4eaf07fd977b13cf495d7c3f76f324b647905daf
Sergio Martins <sergiomos2010@gmail.com>
2021-08-22 23:46:18 -0300

    desafio 17

---
+++ challenges/desafio17.js
@@ -0,0 +1,14 @@
+db.produtos.createIndex(
+  { 
+    descricao: "text", 
+  },
+  { 
+    default_language: "portuguese", 
+  },
+);
+
+db.produtos.countDocuments({ 
+  $text: { 
+    $search: "frango hamburguer", 
+  }, 
+});

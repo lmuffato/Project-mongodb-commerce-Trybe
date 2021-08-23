543bb61a898066783dfc488538153efb7e4f160a
Erick-Marinho <erickgaves@gmail.com>
2021-08-23 17:43:31 -0300

    cumprindo desafio 8

---
+++ challenges/desafio8.js
@@ -0,0 +1,22 @@
+const queryAll = {};
+
+const params = {
+  upsert: true,
+  multi: true,
+};
+
+const projectionFind = {
+  nome: 1, 
+  ingredientes: 1, 
+  _id: 0,
+};
+
+db.produtos.findOneAndUpdate(
+  { nome: "Quarteirão de Queijo" },
+  { $pop: {
+    ingredientes: -1,
+  } },
+  params,
+);
+
+db.produtos.find(queryAll, projectionFind);

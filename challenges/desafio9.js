d94bbb960858d0c52a2c861396ae9fc878a088d5
Erick-Marinho <erickgaves@gmail.com>
2021-08-23 17:47:37 -0300

    cumprindo desafio 9

---
+++ challenges/desafio9.js
@@ -0,0 +1,22 @@
+const queryAll = {};
+
+const params = {
+  upsert: true,
+  multi: true,
+};
+
+const projectionFind = {
+  nome: 1, 
+  ingredientes: 1, 
+  _id: 0,
+};
+
+db.produtos.findOneAndUpdate(
+  { nome: "Cheddar McMelt" },
+  { $pop: {
+    ingredientes: 1,
+  } },
+  params,
+);
+
+db.produtos.find(queryAll, projectionFind);

ec00919ae76e37853b86d1cf60ec5161313e6db5
Willian Alves <willian.alvesb65@gmail.com>
2021-08-24 01:52:33 -0300

    desafio 13.1

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -4,7 +4,7 @@ db.produtos.updateMany(
     { $push: { tags: "muito sódio" } },
   );
   
-  db.produtos.find(
-    {},
-    { _id: 0, nome: 1, tags: 1 },
-  );
+db.produtos.find(
+  {},
+  { _id: 0, nome: 1, tags: 1 },
+);

3bb2f192ca8b886acf248592683c00fade196591
WillianMendes <willianmendesbr@gmail.com>
2021-08-24 06:37:46 -0300

    feat: challenge 2

---
+++ challenges/desafio2.js
@@ -0,0 +1,16 @@
+db.produtos.updateMany(
+  {
+    valorUnitario: null,
+  },
+  {
+    $set: {
+      valorUnitario: NumberDecimal("0.00"),
+    },
+  }
+);
+
+db.produtos.find({}, {
+  _id: 0,
+  nome: 1,
+  valorUnitario: 1,
+});

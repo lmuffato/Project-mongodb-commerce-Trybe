02a37dd01fc2fef739f95f6e0a54c01b6c7dc03a
WillianMendes <willianmendesbr@gmail.com>
2021-08-24 06:55:20 -0300

    feat: challenge 4

---
+++ challenges/desafio4.js
@@ -0,0 +1,20 @@
+db.produtos.updateMany(
+  {
+    nome: "Big Mac",
+  },
+  {
+    $set: {
+      ultimaModificacao: Date(),
+    },
+  },
+);
+
+db.produtos.find(
+  {
+    ultimaModificacao: { $exists: true },
+  },
+  {
+    _id: 0,
+    nome: 1,
+  },
+);

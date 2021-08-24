1df989cc4ecbe49c5ebcfbe473ca2a1bb43e91af
Willian Alves <willian.alvesb65@gmail.com>
2021-08-23 22:44:47 -0300

    desafio 3

---
+++ challenges/desafio3.js
@@ -0,0 +1,19 @@
+// use("commerce")
+db.produtos.updateMany(
+  {},
+  { $set: { avaliacao: NumberInt("0") } },
+);
+db.produros.updateMany(
+  { tag: { $all: ["bovino"] } },
+  { $inc: { avaliacao: 5 } },
+);
+
+db.produtos.updateMany(
+  { tag: { $all: ["ave"] } },
+  { $inc: { avaliacao: 3 } },
+);
+
+db.produros.find(
+  {},
+  { _id: 0, nome: 1, avaliacao: 1 },
+);

b046e9a2ee931c1dc6eb3e83d35c21b9daba9110
Willian Alves <willian.alvesb65@gmail.com>
2021-08-24 11:32:38 -0300

    Fix: Desafio 3.2

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -1,19 +1,19 @@
 db.produtos.updateMany(
   {},
-  { $set: { avaliacao: NumberInt("0") } },
+  { $set: { avaliacao: NumberInt(0) } },
 );
 
-db.produros.updateMany(
-  { tag: { $all: ["bovino"] } },
+db.produtos.updateMany(
+  { tags: { $all: ["bovino"] } },
   { $inc: { avaliacao: 5 } },
 );
 
 db.produtos.updateMany(
-  { tag: { $all: ["ave"] } },
+  { tags: { $all: ["ave"] } },
   { $inc: { avaliacao: 3 } },
 );
 
-db.produros.find(
+db.produtos.find(
   {},
   { _id: 0, nome: 1, avaliacao: 1 },
 );

2f3a27c1109625350fa6f748703397c1696ad0d5
Willian Alves <willian.alvesb65@gmail.com>
2021-08-23 22:53:17 -0300

    Desafio 4

---
+++ challenges/desafio4.js
@@ -0,0 +1,10 @@
+// use("commerce")
+db.produtos.updateOne(
+  { nome: "Big Mac" },
+  { $currentDate: { ultimaModificacao: { $type: "date" } } },
+);
+
+db.produtos.find(
+  { ultimaModificacao: { $exists: true } },
+  { _id: 0, nome: 1 },
+);

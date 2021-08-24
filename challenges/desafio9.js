1f73bdc314e8dcd4d46015285685ef41f1cb2082
Willian Alves <willian.alvesb65@gmail.com>
2021-08-23 23:59:01 -0300

    Desafio 9

---
+++ challenges/desafio9.js
@@ -0,0 +1,10 @@
+// use("commerce")
+db.produtos.updateOne(
+    { nome: "Cheddar McMelt" },
+    { $pop: { ingredientes: 1 } },
+  );
+  
+db.produtos.find(
+  {},
+  { _id: 0, nome: 1, ingredientes: 1 },
+);

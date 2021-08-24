545ce89d3673e6adfe4b4d366eb6c14715138ecd
samuelmmjr <samuelmmjunior@gmail.com>
2021-08-24 12:50:41 -0300

    Feat:req2 correçao valor unitario

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -1,5 +1,6 @@
 db.produtos.updateMany(
-    {}, 
+    { valorUnitario: { $exists: 0 } },
     { $set: { valorUnitario: NumberDecimal("0.00") } },
 );
-db.produtos.find({}, { _id: 0, nome: 1, valorUnitario: 1 });
+db.produtos.find({}, 
+    { valorUnitario: true, nome: true, _id: false });

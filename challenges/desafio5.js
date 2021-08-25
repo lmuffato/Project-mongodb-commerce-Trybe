c1d6d339d9740332f3288de97ba1e9b8dce15d10
lmuffato <lmiremuffato@gmail.com>
2021-08-25 10:57:53 -0300

    requisito 9

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -4,12 +4,13 @@ Para isso, escreva no arquivo desafio5.js duas queries, nesta ordem: /*
 
 /* 1. Crie uma query que adicione ketchup aos ingredientes para todos os sanduíches
 menos o McChicken, garantindo que não haja duplicidade nos ingredientes. */
-db.produtos.updateMany({
-  nome: { $ne: "McChicken" } },
-  { $addToSet: { ingredientes: "ketchup" } });
+db.produtos.updateMany(
+  { nome: { $ne: "McChicken" } },
+  { $addToSet: { ingredientes: "ketchup" } },
+);
 
 /* 2. Crie uma query que retorne o nome e ingredientes de todos os documentos. */
 db.produtos.find(
-  {},
-  { _id: false, nome: true, ingredientes: true },
+  { },
+  { nome: true, ingredientes: true, _id: false },
 );
---
+++ challenges/desafio9.js
@@ -0,0 +1,15 @@
+/* Desafio 9 - Remova o último ingrediente do sanduíche Cheddar McMelt.
+Para isso, escreva no arquivo desafio9.js duas queries, nesta ordem: */
+
+/* 1. Crie uma query que faça a remoção do último ingrediente no
+sanduíche Cheddar McMelt. */
+db.produtos.updateOne(
+  { nome: "Cheddar McMelt" },
+  { $pop: { ingredientes: 1 } },
+  );
+
+/* 2. Crie uma query que retorne o nome e ingredientes de todos os documentos. */
+db.produtos.find(
+  {},
+  { _id: false, nome: true, ingredientes: true },
+);

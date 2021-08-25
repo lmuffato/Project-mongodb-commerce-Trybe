f9d2c5d2a7c41850679d8c3b4c7d47d0c73233e8
lmuffato <lmiremuffato@gmail.com>
2021-08-25 10:48:10 -0300

    requisito 7

--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -1,4 +1,14 @@
-// db.produtos.updateMany(
-//   { nome: "McChicken" },
-//   { $pull: { ingredientes: "ketchup" } },
-//   );
+/* Desafio 7 - Remova o item cebola de todos os sanduíches.
+Para isso, escreva no arquivo desafio7.js duas queries, nesta ordem: */
+
+/* 1. Crie uma query que faça a remoção do item cebola em todos os sanduíches. */
+db.produtos.updateMany(
+  {},
+  { $pull: { ingredientes: "cebola" } },
+  );
+
+/* 2. Crie uma query que retorne o nome e ingredientes de todos os documentos. */
+db.produtos.find(
+  {},
+  { _id: false, nome: true, ingredientes: true },
+);

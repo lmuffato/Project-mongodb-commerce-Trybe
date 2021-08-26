938ed66fc77146afeff7bdf7bee22cea12133770
lmuffato <lmiremuffato@gmail.com>
2021-08-26 11:13:34 -0300

    requisito 19

---
+++ challenges/desafio19.js
@@ -0,0 +1,16 @@
+/* Desafio 19 - Renomeie o campo descricao para descricaoSite em todos os documentos.
+Para isso, escreva no arquivo desafio19.js duas queries, nesta ordem: */
+
+/* 1. Crie uma query que faça a renomeação do campo descricao para descricaoSite
+em todos os documentos. */
+db.produtos.updateMany(
+  {},
+  { $rename: { descricao: "descricaoSite" } },
+);
+
+/* 2. Crie uma query que retorne o nome, descricao e descricaoSite de todos
+os documentos. */
+db.produtos.find(
+  {},
+  { _id: false, descricao: true, descricaoSite: true },
+);

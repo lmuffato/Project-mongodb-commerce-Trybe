39abcfab45c7c28fbf6e814d27ce079868ed5416
Renan Costa <renancosta21@gmail.com>
2021-08-30 02:02:13 -0300

    requisito 13

---
+++ challenges/desafio13.js
@@ -0,0 +1,22 @@
+db.produtos.updateMany(
+  {
+    valoresNutricionais: {
+      $elemMatch: {
+        tipo: "sódio",
+        percentual: {
+          $gte: 40,
+        },
+      },
+    },
+  },
+  {
+    $addToSet: {
+      tags: "muito sódio",
+    },
+  },
+);
+
+db.produtos.find(
+  { },
+  { nome: 1, tags: 1, _id: 0 },
+);

19f6d076b05d4ded4117e07732994887b2aef007
jepaur5 <jepaur5@gmail.com>
2021-09-01 18:05:08 -0300

    requisito14

---
+++ challenges/desafio14.js
@@ -0,0 +1,17 @@
+db.produtos.updateMany(
+  {
+    valoresNutricionais: {
+      $elemMatch: {
+        tipo: "sódio",
+        percentual: { $lt: 40, $gt: 20 },
+      },
+    },
+  },
+  {
+    $addToSet: {
+      tags: "contém sódio",
+    },
+  },
+);
+
+db.produtos.find({}, { _id: 0, nome: 1, tags: 1 });

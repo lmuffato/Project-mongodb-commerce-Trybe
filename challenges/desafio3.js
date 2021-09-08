418d428dcbf16725554ab9b00407480824c9bdbb
Marcos Albuquerque <mpdsa20@gmail.com>
2021-09-08 15:58:29 -0300

    Desafio 3

---
+++ challenges/desafio3.js
@@ -0,0 +1,39 @@
+db.produtos.updateMany(
+  {},
+  {
+    $set: {
+      avaliacao: NumberInt(0),
+    },
+  },
+);
+
+db.produtos.updateMany(
+  {
+    "tags.0": "bovino",
+  },
+  {
+    $set: {
+      avaliacao: NumberInt(5),
+    },
+  },
+);
+
+db.produtos.updateMany(
+  {
+    "tags.0": "ave",
+  },
+  {
+    $set: {
+      avaliacao: NumberInt(3),
+    },
+  },
+);
+
+db.produtos.find(
+  {},
+  {
+    nome: 1,
+    avaliacao: 1,
+    _id: 0,
+  },
+);

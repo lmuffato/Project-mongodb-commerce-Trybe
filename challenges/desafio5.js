c1fef56050ba11ec30347dea6170baf63ec37290
Marcos Albuquerque <mpdsa20@gmail.com>
2021-09-08 18:35:28 -0300

    Desafio 5

---
+++ challenges/desafio5.js
@@ -0,0 +1,21 @@
+db.produtos.updateMany(
+  {
+    nome: {
+      $nin: ["McChicken"],
+    },
+  },
+  {
+    $addToSet: {
+      ingredientes: "ketchup",
+    },
+  },
+);
+
+db.produtos.find(
+  {},
+  {
+    nome: 1,
+    ingredientes: 1,
+    _id: 0,
+  },
+);

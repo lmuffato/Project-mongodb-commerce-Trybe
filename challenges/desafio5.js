3e4379f804e153dc689d48aeeb267a149a0660c3
Paulo Henrique <ph.pereira.7@gmail.com>
2021-09-09 16:16:30 -0300

    Desafio 5:fix

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,5 +1,5 @@
 db.produtos.updateMany(
-    { $ne: { name: "McChicken" } },
+    { name: { $ne: "McChicken" } },
     { $addToSet: { ingredientes: "ketchup" } },
   );
 db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });

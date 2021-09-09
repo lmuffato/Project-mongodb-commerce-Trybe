bbc0e2bac28906d7cb1b2b523c902cdaf6a27846
Paulo Henrique <ph.pereira.7@gmail.com>
2021-09-09 20:25:39 -0300

    Desafio 19

--- challenges/desafio19.js
+++ challenges/desafio19.js
@@ -1,5 +1,5 @@
 db.produtos.updateMany(
     { },
-    { $rename: { "descricao": "descricaoSite" } },
+    { $rename: { descricao: "descricaoSite" } },
 );
 db.produtos.find({}, { _id: 0, nome: 1, descricao: 1, descricaoSite: 1 });

2273c4901f75768fed79d18a0964f4d7ef4df91b
Alan Albuquerque Ferreira Lopes <alan.alb.flopes@gmail.com>
2021-09-09 15:07:54 -0400

    fix: i forgot to save changes again :X

--- challenge10.js
+++ challenge10.js
@@ -12,4 +12,4 @@ db.produtos.updateMany(
     { $push: { vendasPorDia: { $each: [120], $position: 6, $slice: 7 } } },
 );
 
-db.produtos.find({}, { _id: 0, nome: 1, vendasPorDia: 1 }).pretty();
+db.produtos.find({}, { _id: 0, nome: 1, vendasPorDia: 1 });

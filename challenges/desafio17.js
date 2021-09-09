81b7e98c1394ca4f66dd180c2f487a15e40e5f56
Marcos Albuquerque <mpdsa20@gmail.com>
2021-09-09 17:45:44 -0300

    Desafio 17

---
+++ challenges/desafio17.js
@@ -0,0 +1,16 @@
+db.produtos.createIndex(
+  {
+    descricao: "text",
+  },
+  {
+    default_language: "portuguese",
+  },
+);
+
+db.produtos.countDocuments(
+  {
+    $text: {
+      $search: "frango hamburguer",
+    },
+  },
+);

cb7e2487677d755b1a9943cbdaa0d7f2770fd9e8
Andre Sampaio <andresampaiolm@gmail.com>
2021-09-09 16:06:36 -0300

    REQ 04

---
+++ challenges/desafio4.js
@@ -0,0 +1,14 @@
+db.produtos.updateOne({
+  nome: "Big Mac",
+}, {
+  $currentDate: {
+    ultimaModificacao: true,
+  },
+});
+
+db.produtos.find({
+  ultimaModificacao: { $exists: true },
+}, {
+  _id: 0,
+  nome: 1,
+});

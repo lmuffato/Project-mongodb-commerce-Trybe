dc23782b3aeadc290feb006f8e3844cc81fb9555
Alan Albuquerque Ferreira Lopes <alan.alb.flopes@gmail.com>
2021-09-09 14:59:59 -0400

    feat: query that creates sells per day, set number of sells in meals type bread and cow meat and in big mac

---
+++ challenge10.js
@@ -0,0 +1,15 @@
+db.produtos.updateMany(
+    {}, { $set: { vendasPorDia: [0, 0, 0, 0, 0, 0, 0] } },
+);
+
+db.produtos.updateOne(
+    { nome: "Big Mac" }, 
+    { $push: { vendasPorDia: { $each: [60], $position: 3, $slice: 7 } } },
+);
+
+db.produtos.updateMany(
+    { tags: { $in: ["bovino", "pão"] } },
+    { $push: { vendasPorDia: { $each: [120], $position: 6, $slice: 7 } } },
+);
+
+db.produtos.find({}, { _id: 0, nome: 1, vendasPorDia: 1 }).pretty();

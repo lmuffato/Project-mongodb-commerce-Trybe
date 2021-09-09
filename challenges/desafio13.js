9212bbf245bad03d09d1015edb11c83d0d48f7c3
Paulo Henrique <ph.pereira.7@gmail.com>
2021-09-09 18:53:10 -0300

    Desafio 13:fix

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,9 +1,8 @@
 db.produtos.updateMany(
-    { "valoresNutricionais.tipo": "sódio", "valoresNutricionais.percentual": { $gte: 40 } },
+    { "valoresNutricionais.2.percentual": { $gte: 40 } },
     { $push: {
         tags: "muito sódio",
       },
     },
-    { upsert: true },
 );
 db.produtos.find({}, { _id: 0, nome: 1, tags: 1 });

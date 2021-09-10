8e7e9428ee09a92a83deda0dd2e9f7de2821d6de
EdmilsonBernardo <ebedmilson48@gmail.com>
2021-09-09 21:15:28 -0300

    fix: solve identation

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,6 +1,6 @@
 db.produtos.updateMany(
-    { nome: { $ne: "McChicken" } }, 
-    { $addToSet: { ingredientes: "ketchup" } },
+  { nome: { $ne: "McChicken" } },
+  { $addToSet: { ingredientes: "ketchup" } },
 );
 
 db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });
--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,6 +1,6 @@
 db.produtos.updateMany(
-    { nome: { $in: ["Big Mac", "Quarteirão com Queijo"] } },
-    { $addToSet: { ingredientes: "bacon" } },
+  { nome: { $in: ["Big Mac", "Quarteirão com Queijo"] } },
+  { $addToSet: { ingredientes: "bacon" } },
 );
 
 db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });

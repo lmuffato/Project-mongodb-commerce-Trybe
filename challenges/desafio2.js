0aea70eb74601f91ad9ea05d09eb2a078c0c01da
pauloeliezerg <pauloeliezerg@gmail.com>
2021-09-10 01:59:31 -0300

    req04

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -1,6 +1,6 @@
 db.produtos.updateMany(
   {
-    valorUnitario: { $exists: 0 }
+    valorUnitario: { $exists: 0 },
   },
   { $set: { valorUnitario: NumberDecimal("0.00") } },
 );
---
+++ challenges/desafio4.js
@@ -0,0 +1,9 @@
+db.produtos.update(
+  {
+    nome: "Big Mac",
+  },
+  {
+    $currentDate: { ultimaModificacao: true },
+  },
+);
+db.produtos.find({ ultimaModificacao: { $exists: 1 } }, { nome: 1, _id: 0 });

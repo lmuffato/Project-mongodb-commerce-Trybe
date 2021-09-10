9bc3fd64e5786d01974a76d4ef3a84ba8419e522
edersonrdg <edersonrodrigo31@gmail.com>
2021-09-09 22:21:46 -0300

    fix: requisito 7

--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -1,2 +1,2 @@
 db.produtos.updateMany({}, { $pull: { ingredientes: "cebola" } });
-db.produtos.find({}, { _id: 0, ingredientes: 1, nome: 1 });
+db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });

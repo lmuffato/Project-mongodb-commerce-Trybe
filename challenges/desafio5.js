70e2c35aecd05deddf9019811c402cae4a8730e1
pauloeliezerg <pauloeliezerg@gmail.com>
2021-09-10 02:05:35 -0300

    req05

---
+++ challenges/desafio5.js
@@ -0,0 +1,16 @@
+db.produtos.updateMany(
+  {
+    nome: { $ne: "McChicken" },
+  },
+  {
+    $addToSet: { ingredientes: "ketchup" },
+  },
+);
+db.produtos.find(
+  {},
+  {
+    nome: 1,
+    ingredientes: 1,
+    _id: 0,
+  },
+);

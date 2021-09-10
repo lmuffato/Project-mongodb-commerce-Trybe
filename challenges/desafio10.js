6103f3238764171d15b740562fe856325d541b0e
nyxnil <nsmnilsonn@gmail.com>
2021-09-09 22:49:34 -0300

    desafio 9, desafio 10.)

---
+++ challenges/desafio10.js
@@ -0,0 +1,11 @@
+db.produtos.updateMany({},
+  { $set: { vendasPorDia: [0, 0, 0, 0, 0, 0, 0] } });
+
+db.produtos.updateMany({ nome: "Big Mac" },
+  { $set: { "vendasPorDia.3": 60 } });
+
+db.produtos.updateMany({ tags: { $in: ["bovino", "pao"] } },
+  { $set: { "vendasPorDia.6": 120 } });
+
+db.produtos.find({},
+  { _id: 0, nome: 1, vendasPorDia: 1 });

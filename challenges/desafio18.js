82df0474415f3a4882ad519b2767c44605fd0f16
nyxnil <nsmnilsonn@gmail.com>
2021-09-10 02:44:03 -0300

    desafio 19, desafio 20.

--- challenges/desafio18.js
+++ challenges/desafio18.js
@@ -1,2 +1,2 @@
 db.produtos.createIndex({ descricao: "text" }, { default_language: "portuguese" });
-db.produtos.find({ $text: { $search: "\"feito com\"" } });
+db.produtos.count({ $text: { $search: "\"feito com\"" } });
